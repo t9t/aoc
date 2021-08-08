b9d7a887c0baa042fb48de88bae516ef4c6dde30
aoc <aoc>
2021-08-08 19:38:34 +0200

    Benchmarks because why not

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"aoc/year2015"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -14,6 +15,15 @@ func main() {
 	year2015.RegisterAll()
 
 	args := os.Args[1:]
+
+	if len(args) == 1 && args[0] == "benchmark" {
+		err := runBenchmark()
+		if err != nil {
+			fmt.Printf("\nError running benchmark: %v\n", err)
+			os.Exit(4)
+		}
+		return
+	}
 
 	if len(args) != 3 {
 		fatalUsage("")
@@ -28,22 +38,69 @@ func main() {
 		fmt.Printf("Unsupported arguments, Year: %d; Day: %d; Part: %d\n", year, day, part)
 		os.Exit(2)
 	}
-	inputFile := fmt.Sprintf("input/%d-%d.txt", year, day)
-	inputData, err := os.ReadFile(inputFile)
+
+	input, err := readInputData(year, day)
 	if err != nil {
-		fmt.Printf("Could not read input from %q: %v\n", inputFile, err)
+		fmt.Printf("Error reading input data: %v\n", err)
 		os.Exit(3)
 	}
 
 	fmt.Printf("Running Year: %d; Day: %d; Part: %d\n", year, day, part)
 	start := time.Now()
-	result, err := execution(strings.TrimSpace(string(inputData)))
+	result, err := execution(input)
 	if err != nil {
 		fmt.Printf("Error running: %v\n", err)
 		os.Exit(4)
 	}
 
 	fmt.Printf("Result (%v): %v\n", time.Since(start), result)
+}
+
+func runBenchmark() error {
+	sortedSelectors := registry.AllSelectorsSorted()
+
+	begin := time.Now()
+	runTimes := make(map[registry.Selector]time.Duration)
+	for i, selector := range sortedSelectors {
+		fmt.Printf("%s%3d/%3d; %v; running year: %d; day: %d; part: %d", clearLine(),
+			i, len(sortedSelectors), time.Since(begin),
+			selector.Year, selector.Day, selector.Part)
+
+		input, err := readInputData(selector.Year, selector.Day)
+		if err != nil {
+			return err
+		}
+
+		start := time.Now()
+		if _, err := registry.Map[selector](input); err != nil {
+			return fmt.Errorf("error executing %d/%d/%d: %w", selector.Year, selector.Day, selector.Part, err)
+		}
+		runTime := time.Since(start)
+		runTimes[selector] = runTime
+	}
+
+	sort.Slice(sortedSelectors, func(i, j int) bool {
+		l, r := sortedSelectors[i], sortedSelectors[j]
+		lt, rt := runTimes[l], runTimes[r]
+		return lt < rt
+	})
+
+	fmt.Print(clearLine())
+	fmt.Println("| Year | Day | Part | Run time   |")
+	fmt.Println("|------|-----|------|------------|")
+	var totalRunTime time.Duration
+	for _, selector := range sortedSelectors {
+		runTime := runTimes[selector]
+		totalRunTime += runTime
+		fmt.Printf("| %4d | %3d | %4d | %10v | \n", selector.Year, selector.Day, selector.Part, runTime)
+	}
+	fmt.Printf("\nTotal run time: %v\n", totalRunTime)
+	return nil
+}
+
+func clearLine() string {
+	// https://unix.stackexchange.com/a/26592
+	return "\033[1K\r"
 }
 
 func mustParseIntArg(argName, v string) int {
@@ -63,4 +120,13 @@ func fatalUsage(errorMessage string) {
 	fmt.Printf("\t%s <year> <day> <part>\n", os.Args[0])
 
 	os.Exit(1)
+}
+
+func readInputData(year, day int) (string, error) {
+	filename := fmt.Sprintf("input/%d-%d.txt", year, day)
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", fmt.Errorf("cannot read data for %d/%d: %w", year, day, err)
+	}
+	return strings.TrimSpace(string(data)), nil
 }
--- registry/registry.go
+++ registry/registry.go
@@ -1,14 +1,17 @@
 package registry
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Execution func(input string) (string, error)
 
-type selector struct {
-	year, day, part int
+type Selector struct {
+	Year, Day, Part int
 }
 
-var registry map[selector]Execution = make(map[selector]Execution)
+var Map map[Selector]Execution = make(map[Selector]Execution)
 
 func MustRegister(year, day, part int, function Execution) {
 	err := Register(year, day, part, function)
@@ -18,16 +21,37 @@ func MustRegister(year, day, part int, function Execution) {
 }
 
 func Register(year, day, part int, function Execution) error {
-	selector := selector{year: year, day: day, part: part}
-	if _, ok := registry[selector]; ok {
+	selector := Selector{Year: year, Day: day, Part: part}
+	if _, ok := Map[selector]; ok {
 		return fmt.Errorf("%d/%d/%d already registered", year, day, part)
 	}
-	registry[selector] = function
+	Map[selector] = function
 	return nil
 }
 
 func Get(year, day, part int) (Execution, bool) {
-	selector := selector{year: year, day: day, part: part}
-	function, ok := registry[selector]
+	selector := Selector{Year: year, Day: day, Part: part}
+	function, ok := Map[selector]
 	return function, ok
+}
+
+func AllSelectorsSorted() []Selector {
+	all := make([]Selector, len(Map))
+	i := 0
+	for selector := range Map {
+		all[i] = selector
+		i++
+	}
+
+	sort.Slice(all, func(i, j int) bool {
+		a, b := all[i], all[j]
+		if a.Year == b.Year {
+			if a.Day == b.Day {
+				return a.Part < b.Part
+			}
+			return a.Day < b.Day
+		}
+		return a.Year < b.Year
+	})
+	return all
 }
