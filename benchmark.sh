053e0f350ff8204bb6b788f911c222f9b6191a3a
aoc <aoc>
2021-08-09 10:51:02 +0200

    Scripts to fetch inputs and benchmark

---
+++ benchmark.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -eu
+
+./run.sh benchmark
---
+++ fetchinputs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+echo "'session' cookie value: "
+read -s SESH
+
+INPUTDIR="./input"
+
+mkdir -p "${INPUTDIR}"
+for year in {2015..2020}; do
+  for day in {1..25}; do
+    echo "Fetching year ${year}; day ${day}"
+    curl -o "${INPUTDIR}/${year}-${day}.txt" -H"Cookie: session=${SESH}" "https://adventofcode.com/${year}/day/${day}/input" 
+  done
+done
