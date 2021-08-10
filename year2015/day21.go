3fe884f8c973f2904f2756b22f32c0e4971d02c8
aoc <aoc>
2021-08-10 21:35:11 +0200

    2015 day 21 part 2

--- year2015/day21.go
+++ year2015/day21.go
@@ -9,13 +9,23 @@ import (
 )
 
 func Day21Part1(input string) (int, error) {
+	minToWin, _, err := day21(input)
+	return minToWin, err
+}
+
+func Day21Part2(input string) (int, error) {
+	_, maxToLose, err := day21(input)
+	return maxToLose, err
+}
+
+func day21(input string) (minToWin int, maxToLose int, err error) {
 	boss, err := parseBossInput(input)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
 	weapons := []rpgItem{
-		{g: 0, dmg: 0},  // Unarmed
+		// Armor is mandatory
 		{g: 8, dmg: 4},  // Dagger
 		{g: 10, dmg: 5}, // Shortsword
 		{g: 25, dmg: 6}, // Warhammer
@@ -23,6 +33,7 @@ func Day21Part1(input string) (int, error) {
 		{g: 74, dmg: 8}, // Greataxe
 	}
 	armor := []rpgItem{
+		// Armor is optional
 		{g: 0, ac: 0},   // Naked
 		{g: 13, ac: 1},  // Leather
 		{g: 31, ac: 2},  // Chainmail
@@ -31,6 +42,7 @@ func Day21Part1(input string) (int, error) {
 		{g: 102, ac: 5}, // Platemail
 	}
 	rings := []rpgItem{
+		// Rings are optional
 		{g: 0, dmg: 0, ac: 0},   // Poor fellow!
 		{g: 0, dmg: 0, ac: 0},   // Poor fellow!!
 		{g: 25, dmg: 1, ac: 0},  // Damage +1
@@ -40,30 +52,30 @@ func Day21Part1(input string) (int, error) {
 		{g: 40, dmg: 0, ac: 2},  // Defense +2
 		{g: 80, dmg: 0, ac: 3},  // Defense +3
 	}
-	minCost := math.MaxInt32
+
+	minToWin, maxToLose = math.MaxInt32, 0
 	for _, weapon := range weapons {
 		for _, armorItem := range armor {
 			for i, leftRing := range rings {
-				for j := i; j < len(rings); j++ {
+				for j := i + 1; j < len(rings); j++ {
 					rightRing := rings[j]
 					totalDmg := weapon.dmg + armorItem.dmg + leftRing.dmg + rightRing.dmg
 					totalAc := weapon.ac + armorItem.ac + leftRing.ac + rightRing.ac
 					inPlayer := rpgCharacter{hp: 100, dmg: totalDmg, ac: totalAc}
 					_, outBoss, _ := playGame(inPlayer, boss)
-					if playerWon := outBoss.hp <= 0; playerWon {
-						if totalG := weapon.g + armorItem.g + leftRing.g + rightRing.g; totalG < minCost {
-							minCost = totalG
-						}
+
+					totalG := weapon.g + armorItem.g + leftRing.g + rightRing.g
+
+					if playerWon := outBoss.hp <= 0; playerWon && totalG < minToWin {
+						minToWin = totalG
+					} else if !playerWon && totalG > maxToLose {
+						maxToLose = totalG
 					}
 				}
 			}
 		}
 	}
-	return minCost, nil
-}
-
-func Day21Part2(input string) (int, error) {
-	return 0, fmt.Errorf("not implemented")
+	return minToWin, maxToLose, nil
 }
 
 func parseBossInput(input string) (boss rpgCharacter, err error) {
