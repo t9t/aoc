a97552318ea4e488b557746df1069052ee935a16
aoc <aoc>
2021-09-08 11:52:57 +0200

    python/2016 put day1 into separate module

--- python/main.py
+++ python/main.py
@@ -1,28 +1,31 @@
-
-def find_distance(input):
-    directions = input.split(", ")
-    x, y, face = 0, 0, 0
-    for dir in directions:
-        turn = 1 if dir[0] == "R" else -1
-        face += turn
-        if face == -1:
-            face = 3
-        elif face == 4:
-            face = 0
-
-        walk = int(dir[1:])
-        if face == 0:  # North
-            y -= walk
-        elif face == 1:  # East
-            x += walk
-        elif face == 2:  # South
-            y += walk
-        elif face == 3:  # West
-            x -= walk
-    return abs(x)+abs(y)
+from year2016 import day1
+import sys
+from datetime import datetime
 
 
 if __name__ == "__main__":
-    with open("../input/2016/1.txt") as f:
+    args = sys.argv[1:]
+    if len(args) != 3:
+        print("Usage:")
+        print("\t%s <year> <day> <part>\n", sys.argv[0])
+        sys.exit(1)
+
+    year = int(args[0])
+    day = int(args[1])
+    part = int(args[2])
+
+    if year != 2016 or day != 1 or part != 1:
+        print("Unsupported arguments, Year: {0}; Day: {1}; Part: {2}",
+              year, day, part)
+        sys.exit(2)
+
+    with open("../input/{0}/{1}.txt".format(year, day)) as f:
         input = f.read()
-    print("distance:", find_distance(input))
+
+    print("Running Year: {0}; Day: {1}; Part: {1}".format(year, day, part))
+
+    start = datetime.now()
+    result = day1.find_distance(input)
+    end = datetime.now()
+
+    print("Result ({0}): {1}".format(end-start, result))
