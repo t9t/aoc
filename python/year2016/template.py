40205bc34d468157d13859b966c16b24c1cb9331
aoc <aoc>
2021-09-08 18:00:29 +0200

    python/2016 template

---
+++ python/year2016/template.py
@@ -0,0 +1,7 @@
+
+def part1(input):
+    return "not implemented"
+
+
+def part2(input):
+    return "not implemented"
---
+++ python/year2016/test_template.py
@@ -0,0 +1,22 @@
+# I don't understand this import, but I have to do it to make `pyton3 -m unittest` work
+from year2016 import template
+import unittest
+
+
+class Test_Day1(unittest.TestCase):
+    def test_part1(self):
+        cases = {
+            "A": "not implemented",
+            "b": "not implemented",
+            "C": "not implemented",
+        }
+        for [input, expected] in cases.items():
+            with self.subTest(input):
+                self.assertEqual(template.part1(input), expected)
+
+    def test_part2(self):
+        self.assertEqual(template.part2("X"), "not implemented")
+
+
+if __name__ == '__main__':
+    unittest.main()
