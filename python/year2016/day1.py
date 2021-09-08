3a7dfbe12a03221fdaff6c5343aae10d5cfa5a89
aoc <aoc>
2021-09-08 16:16:31 +0200

    python/2016 day 1 simplify

--- python/year2016/day1.py
+++ python/year2016/day1.py
@@ -1,28 +1,13 @@
 
 def part1(input):
-    directions = input.split(", ")
-    x, y, face = 0, 0, 0
-    for dir in directions:
-        turn = 1 if dir[0] == "R" else -1
-        face += turn
-        if face == -1:
-            face = 3
-        elif face == 4:
-            face = 0
-
-        walk = int(dir[1:])
-        if face == 0:  # North
-            y -= walk
-        elif face == 1:  # East
-            x += walk
-        elif face == 2:  # South
-            y += walk
-        elif face == 3:  # West
-            x -= walk
-    return abs(x)+abs(y)
+    return traverse(input, False)
 
 
 def part2(input):
+    return traverse(input, True)
+
+
+def traverse(input, first_double_visited):
     visited = set()
     directions = input.split(", ")
     x, y, face = 0, 0, 0
@@ -45,10 +30,11 @@ def part2(input):
             elif face == 3:  # West
                 x -= 1
 
-            key = "{0},{1}".format(x, y)
-            if key in visited:
-                return abs(int(x))+abs(int(y))
-            else:
-                visited.add(key)
+            if first_double_visited:
+                key = "{0},{1}".format(x, y)
+                if key in visited:
+                    return abs(x)+abs(y)
+                else:
+                    visited.add(key)
 
-    raise Exception("no location visited twice")
+    return abs(x)+abs(y)
