f3d85aa43e76e3290655a898183dcc0f9a90959a
aoc <aoc>
2021-09-08 16:14:49 +0200

    python/2016 day 1 part 2

--- python/year2016/day1.py
+++ python/year2016/day1.py
@@ -20,3 +20,35 @@ def part1(input):
         elif face == 3:  # West
             x -= walk
     return abs(x)+abs(y)
+
+
+def part2(input):
+    visited = set()
+    directions = input.split(", ")
+    x, y, face = 0, 0, 0
+    for dir in directions:
+        turn = 1 if dir[0] == "R" else -1
+        face += turn
+        if face == -1:
+            face = 3
+        elif face == 4:
+            face = 0
+
+        walk = int(dir[1:])
+        for _ in range(walk):
+            if face == 0:  # North
+                y -= 1
+            elif face == 1:  # East
+                x += 1
+            elif face == 2:  # South
+                y += 1
+            elif face == 3:  # West
+                x -= 1
+
+            key = "{0},{1}".format(x, y)
+            if key in visited:
+                return abs(int(x))+abs(int(y))
+            else:
+                visited.add(key)
+
+    raise Exception("no location visited twice")
--- python/year2016/test_day1.py
+++ python/year2016/test_day1.py
@@ -4,14 +4,18 @@ import unittest
 
 
 class Test_Day1(unittest.TestCase):
-    def test_part1_1(self):
-        self.assertEqual(day1.part1("R2, L3"), 5)
+    def test_part1(self):
+        cases = {
+            "R2, L3": 5,
+            "R2, R2, R2": 2,
+            "R5, L5, R5, R3": 12
+        }
+        for [input, expected] in cases.items():
+            with self.subTest(input):
+                self.assertEqual(day1.part1(input), expected)
 
-    def test_part1_2(self):
-        self.assertEqual(day1.part1("R2, R2, R2"), 2)
-
-    def test_part1_3(self):
-        self.assertEqual(day1.part1("R5, L5, R5, R3"), 12)
+    def test_part2(self):
+        self.assertEqual(day1.part2("R8, R4, R4, R8"), 4)
 
 
 if __name__ == '__main__':
