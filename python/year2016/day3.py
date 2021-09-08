2f63571e0868792892ae0febf20a6487b4589d57
aoc <aoc>
2021-09-08 20:13:19 +0200

    python/2016 day 3 part 2

--- python/year2016/day3.py
+++ python/year2016/day3.py
@@ -1,17 +1,22 @@
 
 def part1(input):
     lines = [s for s in input.splitlines() if s]
-    possible = 0
-    for line in lines:
-        [one, two, three] = [int(s.strip()) for s in line.split(" ") if s]
-        if is_maybe_possible_triangle(one, two, three):
-            possible += 1
-
-    return possible
+    triangles = [[int(s.strip()) for s in line.split(" ") if s] for line in lines]
+    return count_maybe_possible_triangles(triangles)
 
 
 def part2(input):
-    return "not implemented"
+    triangles = extract_numbers_columnwise(input)
+    return count_maybe_possible_triangles(triangles)
+
+
+def count_maybe_possible_triangles(l):
+    possible = 0
+    for s in l:
+        if is_maybe_possible_triangle(s[0], s[1], s[2]):
+            possible += 1
+
+    return possible
 
 
 def is_maybe_possible_triangle(one, two, three):
@@ -22,3 +27,20 @@ def is_maybe_possible_triangle(one, two, three):
     if two+three <= one:
         return False
     return True
+
+
+def extract_numbers_columnwise(input):
+    lines = [s for s in input.splitlines() if s]
+    lists = [[], [], []]
+    n = 0
+    for line in lines:
+        nums = [int(s.strip()) for s in line.split(" ") if s]
+        l = lists[n]
+        for i in nums:
+            l.append(i)
+        n = n+1 if n < 2 else 0
+
+    out = list()
+    for i in range(len(lists[0])):
+        out.append([lists[0][i], lists[1][i], lists[2][i]])
+    return out
--- python/year2016/test_day3.py
+++ python/year2016/test_day3.py
@@ -16,12 +16,33 @@ class Test_Day3(unittest.TestCase):
             with self.subTest(input):
                 self.assertEqual(day3.is_maybe_possible_triangle(one, two, three), expected)
 
+    def test_extract_numbers_columnwise(self):
+        input = """ 101 301 501
+                    102 302 502
+                    103 303 503
+                    201 401 601
+                    202 402 602
+                    203 403 603"""
+        actual = day3.extract_numbers_columnwise(input)
+        expected = [[101, 102, 103], [301, 302, 303], [501, 502, 503],
+                    [201, 202, 203], [401, 402, 403], [601, 602, 603]]
+        self.assertEqual(actual, expected)
+
     def test_part1(self):
         input = """  1     2     3 
                      5    10    25 
                      3     4     5 
                     36    77    85 """
         self.assertEqual(day3.part1(input), 2)
+
+    def test_part2(self):
+        input = """  1   5   3 
+                     2  10   4
+                     3  25   5
+                    36   6   9
+                    77   9  40
+                    85  30  41"""
+        self.assertEqual(day3.part2(input), 3)
 
 
 if __name__ == '__main__':
