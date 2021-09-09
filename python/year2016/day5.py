a85ffc57720764aeccd5f08b56797f71939ac844
aoc <aoc>
2021-09-09 20:17:47 +0200

    python/2016 day 5 part 1 multiprocessing

--- python/year2016/day5.py
+++ python/year2016/day5.py
@@ -1,4 +1,5 @@
 import hashlib
+import multiprocessing as mp
 
 
 def part1(input):
@@ -9,18 +10,32 @@ def part2(input):
     return "not implemented"
 
 
-def find_door_password(id: str) -> str:
-    max = 100_000_000
-    n = 0
-    password = ""
-    while True:
-        hash = hashlib.md5((id + str(n)).encode("utf-8"))
-        digest = hash.hexdigest()
-        if digest.startswith("00000"):
-            password += digest[5]
-            if len(password) == 8:
-                return password
-        n += 1
+def calculate_hash(s: str) -> str:
+    return hashlib.md5(s.encode("utf-8")).hexdigest()
 
-        if n >= max:
-            raise Exception("No door password found after {0} iterations".format(max))
+
+def is_matching_id(id: str, n: int) -> str:
+    hash = calculate_hash(id+str(n))
+    if hash.startswith("00000"):
+        return hash[5]
+    return None
+
+
+def find_door_password(id: str) -> str:
+    with mp.Pool(mp.cpu_count()) as pool:
+        max = 100_000_000
+        batch_size = 500_000
+        n = 0
+        password = ""
+        while True:
+            results = pool.starmap(is_matching_id, [(id, i) for i in range(n, n+batch_size)])
+            for r in results:
+                if not r:
+                    continue
+                password += r
+                if len(password) == 8:
+                    return password
+            n += batch_size
+
+            if n >= max:
+                raise Exception("No door password found after {0} iterations".format(max))
