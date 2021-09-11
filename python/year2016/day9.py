4efd971f0727ac3693bb413a6ee737c879fbf441
aoc <aoc>
2021-09-11 16:50:15 +0200

    python/2016 day 9 part 2

--- python/year2016/day9.py
+++ python/year2016/day9.py
@@ -6,7 +6,7 @@ def part1(input: str):
 
 
 def part2(input: str):
-    return "not implemented"
+    return get_decompressed_length_v2(input.strip())
 
 
 regex_marker = r"\((\d+)x(\d+)\)"
@@ -25,6 +25,29 @@ def get_decompressed_length(s: str) -> int:
         marker_len = match.end()
         [data_len, data_times] = [int(s) for s in match.groups()]
         total_length += data_len * data_times
+        i += marker_len+data_len
+
+    return total_length
+
+
+def get_decompressed_length_v2(s: str) -> int:
+    total_length = 0
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c != '(':
+            total_length += 1
+            i += 1
+            continue
+        match = re.search(regex_marker, s[i:])
+        marker_len = match.end()
+        [data_len, data_times] = [int(s) for s in match.groups()]
+        data_start = i+marker_len
+        data = s[data_start:data_start+data_len]
+        actual_data_len = data_len
+        if '(' in data:
+            actual_data_len = get_decompressed_length_v2(data)
+        total_length += actual_data_len * data_times
         i += marker_len+data_len
 
     return total_length
--- python/year2016/test_day9.py
+++ python/year2016/test_day9.py
@@ -18,15 +18,16 @@ class Test_Day9(unittest.TestCase):
             with self.subTest(input):
                 self.assertEqual(day9.get_decompressed_length(input), expected)
 
-    def test_part2(self):
+    def test_get_decompressed_length_v2(self):
         cases = {
-            "A": "not implemented",
-            "b": "not implemented",
-            "C": "not implemented",
+            "(3x3)XYZ": 9,
+            "X(8x2)(3x3)ABCY": 20,
+            "(27x12)(20x12)(13x14)(7x10)(1x12)A": 241920,
+            "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN": 445,
         }
         for [input, expected] in cases.items():
             with self.subTest(input):
-                self.assertEqual(day9.part2(input), expected)
+                self.assertEqual(day9.get_decompressed_length_v2(input), expected)
 
 
 if __name__ == '__main__':
