3dc8cc11666d96d5b25c0765cc05bf06bd36648c
aoc <aoc>
2021-09-11 21:41:17 +0200

    python/2016 day 10 part 2

--- python/year2016/day10.py
+++ python/year2016/day10.py
@@ -6,65 +6,65 @@ regex_action = r"bot (\d+) gives low to (bot|output) (\d+) and high to (bot|outp
 
 
 def part1(input: str):
-    return find_bot_comparing(input, 17, 61)
+    return process_bots(input, find=(17, 61))
 
 
-def find_bot_comparing(input: str, find_low: int, find_high: int) -> int:
+def part2(input: str):
+    return process_bots(input, find=None)
+
+
+def process_bots(input: str, find) -> int:
     bots = {}
+    outputs = {0: [], 1: [], 2: []}
 
     def ensure_bot(num):
         if num not in bots:
-            print("    ~Creating bot", num)
             bots[num] = Bot()
         return bots[num]
 
-    print("Setting intial values")
+    def ensure_output(num):
+        if num not in outputs:
+            outputs[num] = []
+        return outputs[num]
+
     matches = re.finditer(regex_value, input, re.MULTILINE)
     for match in matches:
         value, num = [int(s) for s in match.groups()]
-        print("  Setting bot num", num, "to value", value)
         ensure_bot(num).give(value)
-
-    print("Initial bots:", bots)
-
-    print("Processing instructions")
 
     matches = list(re.finditer(regex_action, input, re.MULTILINE))
     i, max = 1, 100
     while i <= max:
-        print("Iteration", i)
         i += 1
 
         for match in matches:
-            print("  Processing match:", [match])
             src_bot_num, low_out_type, low_out_num, high_out_type, high_out_num = match.groups()
             src_bot_num, low_out_num, high_out_num = int(src_bot_num), int(low_out_num), int(high_out_num)
 
             src_bot = ensure_bot(src_bot_num)
             if not src_bot.has_two():
-                print("    Source bot", src_bot_num, "does not have 2 values:", src_bot)
                 continue
 
             low, high = src_bot.get_and_clear()
-            if low == find_low and high == find_high:
-                print("    Found bot about to give away low", low, "and high", high, ":", src_bot_num)
+            if find != None and low == find[0] and high == find[1]:
                 return src_bot_num
-
-            print("    Bot", src_bot_num, "giving away low", low, "and high", high)
 
             if low_out_type == "bot":
                 ensure_bot(low_out_num).give(low)
-                print("      Gave low", low, "to bot", low_out_num, ":", bots[low_out_num])
+            else:
+                ensure_output(low_out_num).append(low)
 
             if high_out_type == "bot":
                 ensure_bot(high_out_num).give(high)
-                print("      Gave high", high, "to bot", high_out_num, ":", bots[high_out_num])
+            else:
+                ensure_output(high_out_num).append(high)
+
+            if not find:
+                one, two, three = outputs[0], outputs[1], outputs[2]
+                if len(one) > 0 and len(two) > 0 and len(three) > 0:
+                    return one[0] * two[0] * three[0]
 
     raise Exception("Found no answer after {} iterations".format(max))
-
-
-def part2(input: str):
-    return "not implemented"
 
 
 class Bot:
--- python/year2016/test_day10.py
+++ python/year2016/test_day10.py
@@ -4,25 +4,22 @@ import unittest
 
 
 class Test_Day10(unittest.TestCase):
-    def test_find_bot_comparing(self):
-        input = """value 5 goes to bot 2
-            bot 2 gives low to bot 1 and high to bot 0
-            value 3 goes to bot 1
-            bot 1 gives low to output 1 and high to bot 0
-            bot 0 gives low to output 2 and high to output 0
-            value 2 goes to bot 2"""
-        actual = day10.find_bot_comparing("\n".join([s.strip() for s in input.splitlines()]), 2, 5)
+    _input = """value 5 goes to bot 2
+        bot 2 gives low to bot 1 and high to bot 0
+        value 3 goes to bot 1
+        bot 1 gives low to output 1 and high to bot 0
+        bot 0 gives low to output 2 and high to output 0
+        value 2 goes to bot 2"""
+
+    def test_process_bots_find(self):
+        actual = day10.process_bots(self.input(), find=(2, 5))
         self.assertEqual(actual, 2)
 
     def test_part2(self):
-        cases = {
-            "A": "not implemented",
-            "B": "not implemented",
-            "C": "not implemented",
-        }
-        for [input, expected] in cases.items():
-            with self.subTest(input):
-                self.assertEqual(day10.part2(input), expected)
+        self.assertEqual(day10.part2(self.input()), 30)
+
+    def input(self):
+        return "\n".join([s.strip() for s in self._input.splitlines()])
 
 
 if __name__ == '__main__':
