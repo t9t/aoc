d359182d4cd1e8c9e1c85d2b7042516c8bd53ae8
aoc <aoc>
2021-09-11 11:02:26 +0200

    python/2016 day 4 ~I D I O M A T I C~ sort

--- python/year2016/day4.py
+++ python/year2016/day4.py
@@ -1,5 +1,3 @@
-import enum
-import functools
 import re
 
 
@@ -35,15 +33,9 @@ def is_real_room(name, checksum):
             counts[c] += 1
         else:
             counts[c] = 1
-    s = sorted(list(counts.items()), key=functools.cmp_to_key(compare_counts))
+    s = sorted(list(counts.items()), key=lambda i: (-1*i[1], i[0]))
     c = s[0][0] + s[1][0] + s[2][0] + s[3][0] + s[4][0]
     return c == checksum
-
-
-def compare_counts(l, r):
-    if l[1] == r[1]:
-        return 0 if l[0] == r[0] else 1 if l[0] > r[0] else -1
-    return 1 if l[1] < r[1] else -1
 
 
 def shift_very_securely(s: str, n: int) -> str:
