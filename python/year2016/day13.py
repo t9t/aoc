26349f20ff19bcaeccb091f726f1d9dd24fbd748
aoc <aoc>
2021-09-12 19:51:49 +0200

    python/2016 day 13 part 2

--- python/year2016/day13.py
+++ python/year2016/day13.py
@@ -1,16 +1,12 @@
-from __future__ import annotations
-import heapq
-from typing import Protocol, Dict, List, Iterator, Tuple, TypeVar, Optional
-import collections
 from collections import deque
 
 
 def part1(input: str):
-    return min_step_to_reach_location(int(input.strip()), 31, 39)
+    return find_something(int(input.strip()), target=(31, 39))
 
 
 def part2(input: str):
-    return "not implemented"
+    return find_something(int(input.strip()), max_steps=50)
 
 
 def min_step_to_reach_location(magic_number: int, target_x: int, target_y: int):
@@ -28,6 +24,32 @@ def min_step_to_reach_location(magic_number: int, target_x: int, target_y: int):
                 visited.add((nx, ny))
 
     raise Exception("No solution found")
+
+
+def find_something(magic_number: int, target: tuple = None, max_steps: int = None):
+    visited = set([(1, 1)])
+    queue = deque([(1, 1, 1)])
+    within_max_steps = set([(1, 1)])
+
+    while len(queue) > 0:
+        steps, x, y = queue.popleft()
+        if max_steps != None and steps > max_steps:
+            continue
+
+        for nx, ny in get_neighbors(magic_number, x, y):
+            if target != None and nx == target[0] and ny == target[1]:
+                return steps
+
+            if (nx, ny) not in visited:
+                queue.append((steps+1, nx, ny))
+                visited.add((nx, ny))
+                if max_steps != None and steps <= max_steps:
+                    within_max_steps.add((nx, ny))
+
+    if within_max_steps != None:
+        return len(within_max_steps)
+    else:
+        raise Exception("No solution found")
 
 
 def get_neighbors(n: int, x: int, y: int):
--- python/year2016/test_day13.py
+++ python/year2016/test_day13.py
@@ -5,17 +5,10 @@ import unittest
 
 class Test_Day13(unittest.TestCase):
     def test_min_step_to_reach_location(self):
-        self.assertEqual(day13.min_step_to_reach_location(10, 7, 4), 11)
+        self.assertEqual(day13.find_something(10, target=(7, 4)), 11)
 
-    def test_part2(self):
-        cases = {
-            "A": "not implemented",
-            "B": "not implemented",
-            "C": "not implemented",
-        }
-        for [input, expected] in cases.items():
-            with self.subTest(input):
-                self.assertEqual(day13.part2(input), expected)
+    def test_bur(self):
+        self.assertEqual(day13.find_something(10, max_steps=50), 151)
 
 
 if __name__ == '__main__':
