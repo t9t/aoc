484090655bceb9e49e73ef4811c1030f571866ed
aoc <aoc>
2021-09-12 16:29:34 +0200

    python/2016 day 12 performance improvement

--- python/year2016/day12.py
+++ python/year2016/day12.py
@@ -8,29 +8,86 @@ def part2(input: str):
 
 
 def process(input: str, c: int) -> int:
-    instructions = input.strip().splitlines()
-    regs = {'a': 0, 'b': 0, 'c': c, 'd': 0, }
+    lines = input.strip().splitlines()
+
+    reg_nums = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+
+    instructions = list()
+    for line in lines:
+        cmd = line[0:3]
+        vs = line[4:]
+        func, args = None, None
+        if cmd == "cpy":
+            src, tgt = vs.split(" ")
+            tgt_reg = reg_nums[tgt]
+
+            if src in reg_nums:
+                func = cpy_reg
+                args = (tgt_reg, reg_nums[src])
+            else:
+                func = cpy_val
+                args = (tgt_reg, int(src))
+        elif cmd == "inc":
+            func = inc
+            args = (reg_nums[vs])
+        elif cmd == "dec":
+            func = dec
+            args = (reg_nums[vs])
+        elif cmd == "jnz":
+            src, jmp = vs.split(" ")
+            jmp = int(jmp)
+
+            if src == "0":
+                func = jnz_zero
+                args = None
+            elif src in reg_nums:
+                func = jnz_reg
+                args = (reg_nums[src], jmp)
+            else:
+                func = jnz_nonzero_val
+                args = (jmp)
+        instructions.append((func, args))
+
+    regs = [0, 0, c, 0]
 
     ptr = 0
-    while ptr < len(instructions):
-        instruction = instructions[ptr]
-        cmd = instruction[0:3]
-        vs = instruction[4:]
-        jump = 1
+    l = len(instructions)
+    while ptr < l:
+        func, args = instructions[ptr]
+        ptr += func(regs, args)
 
-        if cmd == "cpy":
-            v, reg = vs.split(" ")
-            regs[reg] = regs[v] if v in regs else int(v)
-        elif cmd == "inc":
-            regs[vs] += 1
-        elif cmd == "dec":
-            regs[vs] -= 1
-        elif cmd == "jnz":
-            v, n = vs.split(" ")
-            v = regs[v] if v in regs else int(v)
-            if v != 0:
-                jump = int(n)
+    return regs[0]
 
-        ptr += jump
 
-    return regs['a']
+def cpy_val(regs, args):
+    regs[args[0]] = args[1]
+    return 1
+
+
+def cpy_reg(regs, args):
+    regs[args[0]] = regs[args[1]]
+    return 1
+
+
+def inc(regs, num):
+    regs[num] += 1
+    return 1
+
+
+def dec(regs, num):
+    regs[num] -= 1
+    return 1
+
+
+def jnz_zero(regs, args):
+    return 1
+
+
+def jnz_nonzero_val(regs, args):
+    return args
+
+
+def jnz_reg(regs, args):
+    if regs[args[0]] != 0:
+        return args[1]
+    return 1
