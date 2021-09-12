10a59847369bf80c77974b3526f5a7417daed031
aoc <aoc>
2021-09-12 21:19:00 +0200

    python/2016 day 14 multiprocessing

--- python/year2016/day14.py
+++ python/year2016/day14.py
@@ -1,4 +1,6 @@
 import hashlib
+import math
+import multiprocessing as mp
 
 
 def part1(input: str):
@@ -10,32 +12,47 @@ def part2(input: str):
 
 
 def find_key_index(input: str, stretching: bool) -> int:
-    maxi = 100_000
     keys = 0
-    cache = dict()
+    max_keys = 25_000  # increase on KeyError
+    cache = gen_keys(input, 0, max_keys, stretching)
 
-    for i in range(0, maxi):
-        hash = cache.pop(i) if i in cache else gen_key(input, i, stretching)
-
-        triple = extract_first_triple(hash)
+    for i in range(0, max_keys):
+        triple = extract_first_triple(cache[i])
         if triple is None:
             continue
 
         quintuple = triple[1]*5
         for j in range(i+1, i+1001):
-            if j in cache:
-                other = cache[j]
-            else:
-                other = gen_key(input, j, stretching)
-                cache[j] = other
-
-            if quintuple in other:
+            if quintuple in cache[j]:
                 keys += 1
                 if keys == 64:
                     return i
                 break
 
-    raise Exception("No solution found after {} iterations".format(maxi))
+    raise Exception("No solution found after {} iterations".format(max_keys))
+
+
+def gen_keys(input: str, start: int, total: int, stretching: bool):
+    cpus = mp.cpu_count()
+    batch_size = int(math.ceil(total/cpus))
+
+    with mp.Pool(cpus) as pool:
+        batches = list()
+        for _ in range(cpus):
+            batches.append((input, start, start+batch_size, stretching))
+            start += batch_size
+        out = dict()
+        results = pool.starmap(gen_keys_between, batches)
+        for d in results:
+            out.update(d)
+        return out
+
+
+def gen_keys_between(input: str, min: int, max: int, stretching: bool) -> dict:
+    out = dict()
+    for i in range(min, max):
+        out[i] = gen_key(input, i, stretching)
+    return out
 
 
 def gen_key(input: str, i: int, stretching: bool) -> str:
