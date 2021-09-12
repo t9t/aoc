087210cefd6336a9c292003d70447047776c8939
aoc <aoc>
2021-09-12 15:18:27 +0200

    python/2016 day 11 reimplementation

--- python/year2016/day11.py
+++ python/year2016/day11.py
@@ -3,128 +3,144 @@ import re
 from enum import Enum
 
 from itertools import combinations
+from itertools import chain
 from collections import deque
 from collections import Counter
 
-regex_floor = r"(\w+)(?:-compatible)? (microchip|generator)"
 
-
-def part1(input: str):
+def part1(input: str) -> int:
     floors = read_floors(input)
-    return mininum_moves_to_top_floor(floors)
+    return find_min_number_of_moves_to_reach_the_top_floor(floors)
 
 
-def part2(input: str):
+def part2(input: str) -> int:
     floors = read_floors(input)
-    additional_items = [('elerium', 'generator'), ('elerium', 'microchip'),
-                        ('dilithium', 'generator'), ('dilithium', 'microchip')]
-    for item in additional_items:
-        floors[0].add(item)
-    return mininum_moves_to_top_floor(floors)
+    first = floors[0]
+    first.add((type_generator, "elerium"))
+    first.add((type_microchip, "elerium"))
+    first.add((type_generator, "dilithium"))
+    first.add((type_microchip, "dilithium"))
+    return find_min_number_of_moves_to_reach_the_top_floor(floors)
 
 
-class ItemType(Enum):
-    GENERATOR = "generator"
-    CHIP = "microchip"
+type_microchip = "microchip"
+type_generator = "generator"
+regex_items = r"(\w+)(?:-compatible)? (microchip|generator)"
 
 
-class Item:
-    def __init__(self, type: ItemType, element: str) -> None:
-        self.type = type
-        self.element = element
+def find_min_number_of_moves_to_reach_the_top_floor(floors: list) -> int:
+    # Once the top floor has total_items, we have moved everything
+    total_items = sum(len(floor) for floor in floors)
+
+    start = (0, 0, floors)
+    # Keep a history of visited states, so we don't re-visit them needlessly
+    history = set()
+    # Implement breath-first-search using a queue
+    queue = deque([start])
+
+    while len(queue) > 0:
+        num_moves, level, floors = queue.popleft()
+
+        next_states = generate_next_states(num_moves, level, floors)
+        for state in next_states:
+            key = state_key(state)
+            if key in history:
+                # Skip already visisted states
+                continue
+
+            new_floors = state[2]
+            if len(new_floors[3]) == total_items:
+                # Top floor contains all the items, we're done here
+                return state[0]
+
+            # This is a valid state, add it to the queue to continue our search from that state onwards
+            queue.append(state)
+            history.add(key)
+
+    raise Exception("No solution found")
 
 
-class State:
-    def __init__(self, moves: int, elevator: int, floors: list) -> None:
-        self.moves = moves
-        self.elevator = elevator
-        self.floors = floors
-        self.key = (self.elevator, tuple(tuple(Counter(type for _, type in floor).most_common())
-                                         for floor in self.floors))
-
-    def generate_nexts(self):
-        this_floor = self.floors[self.elevator]
-        doubles = list(combinations(this_floor, 2))  # Move 2 items at once
-        singles = list(combinations(this_floor, 1))  # Move just a single item
-        movables = set(doubles + singles)  # All possible item combinations we could move
-        directions = list()
-        if self.elevator > 0:
-            if len(self.floors[self.elevator-1]) != 0:
-                # No point going down to empty floors
-                directions.append(-1)
-        if self.elevator < len(self.floors)-1:
-            directions.append(1)
-
-        states = list()
-        for items_to_move in movables:
-            for direction in directions:
-                move_to = self.elevator + direction
-
-                unsafe = False
-                new_floors = list()
-                for i, floor in enumerate(self.floors):
-                    cp = floor.copy()
-                    if i == self.elevator:
-                        for item in items_to_move:
-                            cp.remove(item)
-                    elif i == move_to:
-                        for item in items_to_move:
-                            cp.add(item)
-                    if not is_safe(cp):
-                        unsafe = True
-                        break
-                    new_floors.append(cp)
-
-                if not unsafe:
-                    states.append(State(self.moves + 1, move_to, new_floors))
-        return states
-
-
-def read_floors(input: str):
+def read_floors(input: str) -> list:
     floors = list()
     for line in input.strip().splitlines():
-        floors.append(set(re.findall(regex_floor, line)))
+        floor = set()
+        matches = re.finditer(regex_items, line)
+        for match in matches:
+            element, type = match.groups()
+            floor.add((type, element))
+
+        floors.append(floor)
     return floors
 
 
-def is_safe(floor: list):
-    # No items or just 1 item is always safe
+def generate_next_states(num_moves: int, current_level: int, floors: list) -> list:
+    current_floor = floors[current_level]
+    single_items = combinations(current_floor, 1)
+    double_items = combinations(current_floor, 2)
+    # We can either move only a single item or two items at the same time
+    move_options = list(chain(single_items, double_items))
+
+    next_states = list()
+    for direction in (-1, 1):  # either 1 down or 1 up
+        other_level = current_level + direction
+        # if direction == -1 and len(floors[other_level]) == 0:
+        # Don't bring stuff down to an empty floor
+        #    continue
+        if other_level < 0 or other_level >= len(floors):
+            continue
+
+        for option in move_options:
+            # Make sure to copy floors to not accidentally modify references to stored lists
+            new_floors = [floor.copy() for floor in floors]
+            # Move items in the move option from this level to the other level
+            for item in option:
+                new_floors[current_level].remove(item)
+                new_floors[other_level].add(item)
+
+            # If either of the new floor configuration is unsafe, don't consider it
+            if is_safe(new_floors[current_level]) and is_safe(new_floors[other_level]):
+                new_state = (num_moves+1, other_level, new_floors)
+                next_states.append(new_state)
+
+    return next_states
+
+
+def is_safe(floor: set) -> bool:
     if len(floor) <= 1:
+        # If there are 0 or 1 items, it's safe anyway
         return True
 
     types = set()
     for item in floor:
-        types.add(item[1])
-        if len(types) == 2:
-            break
+        types.add(item[0])
 
-    # If we only have generators or only chips, it's safe
     if len(types) <= 1:
+        # If there is just 1 item type, it is always safe
         return True
 
-    chips = set([item[0] for item in floor if item[1] == "microchip"])
-    generators = set([item[0] for item in floor if item[1] == "generator"])
-
-    for chip in chips:
-        if chip not in generators:
-            # If we have a chip without generator, it will take radiation from another generator -> unsafe
+    # When there is a mix, each microchip should be in a generator, otherwise another generator will break it
+    chips, generators = items_by_type(floor)
+    for element in chips:
+        if element not in generators:
             return False
     return True
 
 
-def mininum_moves_to_top_floor(floors):
-    initial = State(0, 0, floors)
-    queue = deque([initial])
-    history = set([initial])
-    total_items = sum([len(floor) for floor in floors])
-    top_floor = len(floors)-1
+def state_key(state) -> tuple:
+    return (state[1], floors_key(state[2]))
 
-    while queue:
-        state = queue.popleft()
-        for next in state.generate_nexts():
-            if len(next.floors[top_floor]) == total_items:
-                return next.moves
 
-            if next.key not in history:
-                history.add(next.key)
-                queue.append(next)
+def floors_key(floors: set) -> str:
+    return ",".join(str(floor_key(floor)) for floor in floors)
+
+
+def floor_key(floor: list) -> tuple:
+    # Any floor with the same number of each item type represents an identical state
+    chips, generators = items_by_type(floor)
+    return (len(chips), len(generators))
+
+
+def items_by_type(floor):
+    chips = [item[1] for item in floor if item[0] == type_microchip]
+    generators = [item[1] for item in floor if item[0] == type_generator]
+    return chips, generators
--- python/year2016/test_day11.py
+++ python/year2016/test_day11.py
@@ -5,17 +5,21 @@ import unittest
 
 class Test_Day11(unittest.TestCase):
     def test_part1(self):
-        self.assertEqual(day11.part1("X"), "not implemented")
+        input = """The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+                    The second floor contains a hydrogen generator.
+                    The third floor contains a lithium generator.
+                    The fourth floor contains nothing relevant."""
+        self.assertEqual(day11.part1(self.strip_lines(input)), 11)
 
     def test_part2(self):
-        cases = {
-            "A": "not implemented",
-            "B": "not implemented",
-            "C": "not implemented",
-        }
-        for [input, expected] in cases.items():
-            with self.subTest(input):
-                self.assertEqual(day11.part2(input), expected)
+        input = """The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+                    The second floor contains a hydrogen generator and a lithium generator.
+                    The third floor contains nothing relevant.
+                    The fourth floor contains nothing relevant."""
+        self.assertEqual(day11.part2(self.strip_lines(input)), 35)
+
+    def strip_lines(self, input):
+        return "\n".join([s.strip() for s in input.strip().splitlines()])
 
 
 if __name__ == '__main__':
