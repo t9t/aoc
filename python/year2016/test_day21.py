3c16c357eb48afc68fd15098204b0caddb645111
aoc <aoc>
2021-09-16 21:12:23 +0200

    python/2016 day 21 part 1 unit tests

--- python/year2016/test_day21.py
+++ python/year2016/test_day21.py
@@ -27,6 +27,39 @@ rotate based on position of letter d
             with self.subTest(input):
                 self.assertEqual(day21.part2(input), expected)
 
+    def test_password_swap_pos(self):
+        self.assertEqual(day21.password_swap_pos("abcde", (1, 3)), "adcbe")
+
+    def test_password_swap_letter(self):
+        self.assertEqual(day21.password_swap_letter("abcde", ('a', 'c')), "cbade")
+
+    def test_password_rotate_lr_left(self):
+        self.assertEqual(day21.password_rotate_lr("abcde", ("left", 2)), "cdeab")
+
+    def test_password_rotate_lr_left_too_much(self):
+        self.assertEqual(day21.password_rotate_lr("abcde", ("left", 8)), "deabc")
+
+    def test_password_rotate_lr_right(self):
+        self.assertEqual(day21.password_rotate_lr("abcde", ("right", 2)), "deabc")
+
+    def test_password_rotate_lr_right_too_much(self):
+        self.assertEqual(day21.password_rotate_lr("abcde", ("right", 8)), "cdeab")
+
+    def test_password_rotate_pos_below_4(self):
+        self.assertEqual(day21.password_rotate_pos("abdec", ('b')), "ecabd")
+
+    def test_password_rotate_pos_4(self):
+        self.assertEqual(day21.password_rotate_pos("ecabd", ('d')), "decab")
+
+    def test_password_reverse(self):
+        self.assertEqual(day21.password_reverse("abcdefg", (2, 5)), "abfedcg")
+
+    def test_password_move(self):
+        self.assertEqual(day21.password_move("bcdea", (1, 4)), "bdeac")
+
+    def test_password_move_backwards(self):
+        self.assertEqual(day21.password_move("bdeac", (3, 0)), "abdec")
+
 
 if __name__ == '__main__':
     unittest.main()
