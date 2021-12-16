596c46316e6f39695c8d3225eba601303c0b88c9
aoc <aoc>
2021-12-16 14:36:05 +0100

    swift/2021 day 16 part 2

--- swift/Sources/aoc/day16.swift
+++ swift/Sources/aoc/day16.swift
@@ -13,43 +13,75 @@ class Day16: Day {
 
 
     func part2() -> Int {
-        return 1337
+        let buf = Buffer(Day16.allHexToBin(input))
+        // Assumption: outer packet is always an operator packet
+        return readPacketVersionAndValue(buf).1
     }
 
     private func readAllVersionNumbersSum(_ bin: String) -> Int {
         let buf = Buffer(bin)
         var versionNumbersSum = 0
         while buf.hasMore() {
-            versionNumbersSum += readVersionOfOnePacket(buf)
+            versionNumbersSum += readPacketVersionAndValue(buf).0
         }
         return versionNumbersSum
     }
 
-    private func readVersionOfOnePacket(_ buf: Buffer) -> Int {
-        var versionNumbersSum = binToInt(buf.advance(by: 3))
+    private func readPacketVersionAndValue(_ buf: Buffer) -> (Int, Int) {
+        let version = binToInt(buf.advance(by: 3))
         let typeId = binToInt(buf.advance(by: 3))
 
         if typeId == 4 /* literal value */ {
+            var numberBits = ""
             var prefix = "1"
             while prefix != "0" {
                 prefix = buf.advance(by: 1)
-                let _ = buf.advance(by: 4) // Number bits
+                numberBits += buf.advance(by: 4)
+            }
+            return (version, binToInt(numberBits))
+        }
+
+        var subValues = Array<Int>()
+        var versionNumbersSum = version
+        let lengthTypeId = buf.advance(by: 1)
+        if lengthTypeId == "0" {
+            let totalLengthInBits = binToInt(buf.advance(by: 15))
+            let subPackets = buf.advance(by: totalLengthInBits)
+
+            let subBuf = Buffer(subPackets)
+            while subBuf.hasMore() {
+                let (subVersion, subValue) = readPacketVersionAndValue(subBuf)
+                versionNumbersSum += subVersion
+                subValues.append(subValue)
             }
         } else {
-            let lengthTypeId = buf.advance(by: 1)
-            if lengthTypeId == "0" {
-                let totalLengthInBits = binToInt(buf.advance(by: 15))
-                let subPackets = buf.advance(by: totalLengthInBits)
-                versionNumbersSum += readAllVersionNumbersSum(subPackets)
-            } else {
-                let numberOfSubPackets = binToInt(buf.advance(by: 11))
+            let numberOfSubPackets = binToInt(buf.advance(by: 11))
 
-                for _ in 1...numberOfSubPackets {
-                    versionNumbersSum += readVersionOfOnePacket(buf)
-                }
+            for _ in 1...numberOfSubPackets {
+                let (subVersion, subValue) = readPacketVersionAndValue(buf)
+                versionNumbersSum += subVersion
+                subValues.append(subValue)
             }
         }
-        return versionNumbersSum
+        let value: Int
+        if typeId == 0 /* sum */ {
+            value = subValues.reduce(0, +)
+        } else if typeId == 1 /* product */ {
+            value = subValues.reduce(1, *)
+        } else if typeId == 2 /* minimum */ {
+            value = subValues.min()!
+        } else if typeId == 3 /* maximum */ {
+            value = subValues.max()!
+        } else if typeId == 5 /* greater than */ {
+            value = subValues[0] > subValues[1] ? 1 : 0
+        } else if typeId == 6 /* less than */ {
+            value = subValues[0] < subValues[1] ? 1 : 0
+        } else if typeId == 7 /* equal to */ {
+            value = subValues[0] == subValues[1] ? 1 : 0
+        } else {
+            fatalError("invalid typeId \(typeId)")
+        }
+        return (versionNumbersSum, value)
     }
 
     private func binToInt(_ bin: String) -> Int {
@@ -68,11 +100,15 @@ class Day16: Day {
         hex.map(Day16.hexCharToBin).joined()
     }
 
-    private class Buffer {
+    private class Buffer: CustomStringConvertible {
         private var remaining: String
 
         init(_ remaining: String) {
             self.remaining = remaining
+        }
+
+        var description: String {
+            "Buffer(remaining: \(remaining))"
         }
 
         func advance(by: Int) -> String {
