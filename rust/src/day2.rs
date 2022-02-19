6823a5ba8f36b3e00494d61fb7e2912bd347fbe1
aoc <aoc>
2022-02-19 12:27:54 +0100

    rust/2017 day 2 part 2

--- rust/src/day2.rs
+++ rust/src/day2.rs
@@ -1,7 +1,6 @@
 pub fn part1(s: &str) -> i32 {
-    let lines = s.split("\n");
     let mut sum = 0;
-    for line in lines {
+    for line in s.lines() {
         let nums = line.split("\t");
         let mut highest = i32::MIN;
         let mut lowest = i32::MAX;
@@ -20,25 +19,41 @@ pub fn part1(s: &str) -> i32 {
     return sum;
 }
 
-pub fn part2(_s: &str) -> i32 {
-    return 5521;
+pub fn part2(s: &str) -> i32 {
+    let mut sum = 0;
+    for line in s.lines() {
+        let nums: Vec<i32> = line
+            .split("\t")
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+        for (i, n) in nums.iter().enumerate() {
+            let left = *n;
+            for j in i + 1..nums.len() {
+                let right = nums[j];
+                let h = if left > right { left } else { right };
+                let l = if left > right { right } else { left };
+                if h % l == 0 {
+                    sum += h / l;
+                }
+            }
+        }
+    }
+    return sum;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static INPUT: &str = "5\t1\t9\t5
-7\t5\t3
-2\t4\t6\t8";
-
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 18);
+        let input = "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8";
+        assert_eq!(part1(input), 18);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT), 5521);
+        let input = "5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5";
+        assert_eq!(part2(input), 9);
     }
 }
