3af283323525963b903919a4697fe3bfd6aabdef
aoc <aoc>
2022-02-19 10:54:34 +0100

    rust/2017 day/part command line args

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,25 @@
+use std::env;
 use std::fs;
 use std::io;
 
 fn main() {
-    let s = read_input(2017, 1).unwrap();
-    println!("sum: {}", calc_sum(&s.trim()))
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let day = args[1].parse::<u8>().unwrap();
+    let part = args[2].parse::<u8>().unwrap();
+
+    println!("Day: {}; part: {}", day, part);
+
+    let input = read_input(2017, day).unwrap();
+    let s = &input.trim();
+    let output: u32;
+    if part == 1 {
+        output = calc_sum(s);
+    } else {
+        output = calc_sum2(s);
+    }
+    println!("Result: {}", output);
 }
 
 fn read_input(year: u16, day: u8) -> io::Result<String> {
