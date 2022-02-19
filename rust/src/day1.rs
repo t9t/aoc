65b237967f70000f0f9cef906979a019a08ec751
aoc <aoc>
2022-02-19 11:22:44 +0100

    rust/2017 run day/part "dynamically"

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,21 +1,21 @@
-pub fn calc_sum(s: &str) -> u32 {
-    return calc_sum_x(s, 1);
+pub fn part1(s: &str) -> i32 {
+    return calc_sum(s, 1);
 }
 
-pub fn calc_sum2(s: &str) -> u32 {
+pub fn part2(s: &str) -> i32 {
     let l = s.len();
-    return calc_sum_x(s, l / 2);
+    return calc_sum(s, l / 2);
 }
 
-fn calc_sum_x(s: &str, x: usize) -> u32 {
+fn calc_sum(s: &str, x: usize) -> i32 {
     let chars: Vec<char> = s.chars().collect();
     let len = chars.len();
-    let mut sum = 0;
+    let mut sum: i32 = 0;
     for (i, c) in chars.iter().enumerate() {
         let next_pos = (i + x) % len;
         let next = chars[next_pos];
         if *c == next {
-            sum += c.to_digit(10).unwrap()
+            sum += c.to_digit(10).unwrap() as i32
         }
     }
     return sum;
@@ -26,19 +26,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_calc_sum() {
-        assert_eq!(calc_sum("1122"), 3);
-        assert_eq!(calc_sum("1111"), 4);
-        assert_eq!(calc_sum("1234"), 0);
-        assert_eq!(calc_sum("91212129"), 9);
+    fn test_part1() {
+        assert_eq!(part1("1122"), 3);
+        assert_eq!(part1("1111"), 4);
+        assert_eq!(part1("1234"), 0);
+        assert_eq!(part1("91212129"), 9);
     }
 
     #[test]
-    fn test_calc_sum2() {
-        assert_eq!(calc_sum2("1212"), 6);
-        assert_eq!(calc_sum2("1221"), 0);
-        assert_eq!(calc_sum2("123425"), 4);
-        assert_eq!(calc_sum2("123123"), 12);
-        assert_eq!(calc_sum2("12131415"), 4);
+    fn test_part2() {
+        assert_eq!(part2("1212"), 6);
+        assert_eq!(part2("1221"), 0);
+        assert_eq!(part2("123425"), 4);
+        assert_eq!(part2("123123"), 12);
+        assert_eq!(part2("12131415"), 4);
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,23 +5,16 @@ use std::io;
 mod day1;
 
 fn main() {
+    let funs = [day1::part1, day1::part2];
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let day = args[1].parse::<u8>().unwrap();
     let part = args[2].parse::<u8>().unwrap();
 
     println!("Day: {}; part: {}", day, part);
-
     let input = read_input(2017, day).unwrap();
-    let s = &input.trim();
-    let output: u32;
-    if part == 1 {
-        output = day1::calc_sum(s);
-    } else {
-        output = day1::calc_sum2(s);
-    }
-    println!("Result: {}", output);
+    let fun = funs[(((day - 1) * 2) + part - 1) as usize];
+    println!("Result: {}", fun(input.trim()));
 }
 
 fn read_input(year: u16, day: u8) -> io::Result<String> {
