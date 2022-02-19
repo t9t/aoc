bf6f3d834be7aab0de9cbbe7c9b6da341ece5617
aoc <aoc>
2022-02-19 10:04:08 +0100

    rust/2017 day 1 part 1 dirty

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "1122";
+    let chars: Vec<char> = s.chars().collect();
+    let mut sum = 0;
+    for (i, c) in chars.iter().enumerate() {
+        let next: char = if i == chars.len() - 1 {
+            chars[0]
+        } else {
+            chars[i + 1]
+        };
+        if *c == next {
+            sum += c.to_digit(10).unwrap()
+        }
+    }
+    println!("sum: {}", sum)
 }
