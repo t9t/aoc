cb5d3c420655f5ab3cba0f7e0d3401dc68c29247
aoc <aoc>
2022-02-22 19:38:24 +0100

    rust/2017 day 5 part 2

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -1,31 +1,32 @@
 use std::error::Error;
 
 pub fn part1(s: &str) -> Result<i32, Box<dyn Error>> {
-    let mut lines: Vec<i32> = s.lines().map(|l| l.parse::<i32>().unwrap()).collect();
-
-    let mut offset: i32 = 0;
-    let mut steps = 0;
-    loop {
-        if offset < 0 || offset >= lines.len() as i32 {
-            return Ok(steps);
-        }
-        let jmp = lines[offset as usize];
-        lines[offset as usize] = jmp + 1;
-        offset += jmp;
-        steps += 1;
-    }
+    return steps_to_exit(s, |_| 1);
 }
 
-pub fn part2(_s: &str) -> Result<i32, Box<dyn Error>> {
-    return Ok(5521);
+pub fn part2(s: &str) -> Result<i32, Box<dyn Error>> {
+    return steps_to_exit(s, |offset| if offset >= 3 { -1 } else { 1 });
+}
+
+fn steps_to_exit(s: &str, d: fn(i32) -> i32) -> Result<i32, Box<dyn Error>> {
+    let mut lines: Vec<i32> = s.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+
+    let mut pos: i32 = 0;
+    let mut steps = 0;
+    loop {
+        if pos < 0 || pos >= lines.len() as i32 {
+            return Ok(steps);
+        }
+        let offset = lines[pos as usize];
+        lines[pos as usize] += d(offset);
+        pos += offset;
+        steps += 1;
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    static INPUT: &str = "bla
-bla";
 
     #[test]
     fn test_part1() {
@@ -34,6 +35,6 @@ bla";
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT).unwrap(), 5521);
+        assert_eq!(part2("0\n3\n0\n1\n-3").unwrap(), 10);
     }
 }
