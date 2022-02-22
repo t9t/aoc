1d66b60969cd5e4cebfb33b1ef5f7d1b2810baaa
aoc <aoc>
2022-02-22 19:19:04 +0100

    rust newday.sh better formatting

--- rust/newday.sh
+++ rust/newday.sh
@@ -16,6 +16,6 @@ MAIN_FILE="${SOURCE_DIR}/main.rs"
 cp -n "${SOURCE_DIR}/template.rs" "${SOURCE_FILE}"
 
 sed -i "s/\/\*mod newday\*\//mod day${DAY};\n\/\*mod newday\*\//" "${MAIN_FILE}"
-sed -i "s/, \/\*newday\*\//, day${DAY}::part1, day${DAY}::part2 \/\*newday\*\//" "${MAIN_FILE}"
+sed -i "s/, \/\*newday\*\//,\n        day${DAY}::part1,\n        day${DAY}::part2, \/\*newday\*\//" "${MAIN_FILE}"
 
-git add "${SOURCE_FILE}" "${MAIN_FILE}"
+#git add "${SOURCE_FILE}" "${MAIN_FILE}"
