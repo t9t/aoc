74fca7ea2d1b88f1910ee9c54601b5683a597530
aoc <aoc>
2022-02-23 19:20:01 +0100

    rust/2017 day 8 part 2

--- rust/src/day8.rs
+++ rust/src/day8.rs
@@ -2,8 +2,17 @@ use std::collections::HashMap;
 use std::error::Error;
 
 pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
+    return Ok(format!("{}", find_highests(s)?.0));
+}
+
+pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
+    return Ok(format!("{}", find_highests(s)?.1));
+}
+
+pub fn find_highests(s: &str) -> Result<(i32, i32), Box<dyn Error>> {
     let mut regs: HashMap<&str, i32> = HashMap::new();
 
+    let mut highest_ever = i32::MIN;
     for line in s.lines() {
         let a = line.split(" ").collect::<Vec<&str>>();
         let reg = a[0];
@@ -25,35 +34,33 @@ pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
         }?;
         if condition_result {
             let reg_v = *regs.get(reg).unwrap_or(&0);
-            regs.insert(reg, reg_v + v * if op == "dec" { -1 } else { 1 });
+            let new_v = reg_v + v * if op == "dec" { -1 } else { 1 };
+            if new_v > highest_ever {
+                highest_ever = new_v;
+            }
+            regs.insert(reg, new_v);
         }
     }
 
-    return Ok(format!("{}", regs.values().max().unwrap_or(&0)));
-}
-
-pub fn part2(_s: &str) -> Result<String, Box<dyn Error>> {
-    return Ok(format!("{}", 5521));
+    return Ok((*regs.values().max().unwrap_or(&0), highest_ever));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static INPUT: &str = "bla
-bla";
-
-    #[test]
-    fn test_part1() {
-        let input = "b inc 5 if a > 1
+    static INPUT: &str = "b inc 5 if a > 1
 a inc 1 if b < 5
 c dec -10 if a >= 1
 c inc -20 if c == 10";
-        assert_eq!(part1(input).unwrap(), "1");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT).unwrap(), "1");
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT).unwrap(), "5521");
+        assert_eq!(part2(INPUT).unwrap(), "10");
     }
 }
