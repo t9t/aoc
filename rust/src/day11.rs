9dbb31ce088b1bb87e6a4898cf73a2bf205feca7
aoc <aoc>
2022-02-26 20:54:46 +0100

    rust/2017 day 11 part 2

--- rust/src/day11.rs
+++ rust/src/day11.rs
@@ -1,21 +1,25 @@
 use std::error::Error;
 
 pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
-    let route = s.split(",").collect();
-    let (tx, ty, tz) = find_target(route)?;
-    let steps = (tx.abs() + ty.abs() + tz.abs()) / 2;
+    let (steps, _) = find_steps(s)?;
 
     return Ok(format!("{}", steps));
 }
 
-pub fn part2(_s: &str) -> Result<String, Box<dyn Error>> {
-    return Ok(format!("{}", 5521));
+pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
+    let (_, furthest) = find_steps(s)?;
+
+    return Ok(format!("{}", furthest));
 }
 
-fn find_target(route: Vec<&str>) -> Result<(i32, i32, i32), Box<dyn Error>> {
-    let mut x = 0;
-    let mut y = 0;
-    let mut z = 0;
+fn find_steps(s: &str) -> Result<(i32, i32), Box<dyn Error>> {
+    let route: Vec<&str> = s.split(",").collect();
+
+    let mut x = 0 as i32;
+    let mut y = 0 as i32;
+    let mut z = 0 as i32;
+    let mut step_count = 0;
+    let mut furthest = 0;
     for step in route {
         match step {
             "n" => {
@@ -44,16 +48,17 @@ fn find_target(route: Vec<&str>) -> Result<(i32, i32, i32), Box<dyn Error>> {
             }
             _ => Err(format!("Invalid direction {}", step))?,
         }
+        step_count = (x.abs() + y.abs() + z.abs()) / 2;
+        if step_count > furthest {
+            furthest = step_count;
+        }
     }
-    return Ok((x, y, z));
+    return Ok((step_count, furthest));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    static INPUT: &str = "bla
-bla";
 
     #[test]
     fn test_part1() {
@@ -65,6 +70,9 @@ bla";
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT).unwrap(), "5521");
+        assert_eq!(part2("ne,ne,ne").unwrap(), "3");
+        assert_eq!(part2("ne,ne,sw,sw").unwrap(), "2");
+        assert_eq!(part2("ne,ne,s,s").unwrap(), "2");
+        assert_eq!(part2("se,sw,se,sw,sw").unwrap(), "3");
     }
 }
