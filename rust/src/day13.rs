a8ff4b8e68cdf827f394d600858f06faf5a0075a
aoc <aoc>
2022-02-27 12:45:57 +0100

    rust/2017 day 13 part 2 faster

--- rust/src/day13.rs
+++ rust/src/day13.rs
@@ -3,14 +3,34 @@ use std::error::Error;
 
 pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
     let (scanners, max_depth) = parse_scanners(s)?;
-    return Ok(format!("{}", severity(&scanners, max_depth, 0).0));
+    let mut severity = 0;
+    for depth in 0..max_depth + 1 {
+        let t = depth;
+        if let Some(range) = scanners.get(&depth) {
+            if t % ((range - 1) * 2) == 0 {
+                severity += range * depth;
+            }
+        }
+    }
+    return Ok(format!("{}", severity));
 }
 
 pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
     let (scanners, max_depth) = parse_scanners(s)?;
     let mut delay = 0;
     loop {
-        if severity(&scanners, max_depth, delay).1 == 0 {
+        let mut hit = false;
+        for depth in 0..max_depth + 1 {
+            let t = delay + depth;
+            if let Some(range) = scanners.get(&depth) {
+                if t % ((range - 1) * 2) == 0 {
+                    hit = true;
+                    break;
+                }
+            }
+        }
+
+        if !hit {
             return Ok(format!("{}", delay));
         }
         delay += 1;
@@ -29,21 +49,6 @@ fn parse_scanners(s: &str) -> Result<(HashMap<u32, u32>, u32), Box<dyn Error>> {
         max_depth = max_depth.max(nums[0]);
     }
     return Ok((scanners, max_depth));
-}
-
-fn severity(scanners: &HashMap<u32, u32>, max_depth: u32, delay: u32) -> (u32, u32) {
-    let mut severity = 0;
-    let mut hit_count = 0;
-    for depth in 0..max_depth + 1 {
-        let t = delay + depth;
-        if let Some(range) = scanners.get(&depth) {
-            if t % ((range - 1) * 2) == 0 {
-                severity += range * depth;
-                hit_count += 1;
-            }
-        }
-    }
-    return (severity, hit_count);
 }
 
 #[cfg(test)]
