adddce1f9dc79941f5ffb04ead2884f91c0254a2
aoc <aoc>
2022-03-02 11:58:10 +0100

    rust/2017 day 18 part 1 cleaned up

--- rust/src/day18.rs
+++ rust/src/day18.rs
@@ -3,59 +3,15 @@ use std::collections::VecDeque;
 use std::error::Error;
 
 pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
-    let mut regs: HashMap<char, i64> = HashMap::new();
-
     let lines = s.lines().collect::<Vec<&str>>();
-    let mut pos: i64 = 0;
-    let mut last_frequency = 0;
-    loop {
-        if pos < 0 || pos >= lines.len() as i64 {
-            Err("Jumped outside of range")?
-        }
-        let line = lines[pos as usize];
-        let mut split = line.split(" ");
-        let op = split.next().unwrap();
-        let reg = split.next().unwrap().chars().next().unwrap();
-        let x = *regs.get(&reg).unwrap_or(&0);
-        let values = split.next().unwrap_or("0");
-        let value = values.parse::<i64>();
-        let val = if value.is_ok() {
-            value?
-        } else {
-            *regs.get(&values.chars().next().unwrap()).unwrap_or(&0)
-        };
-
-        match op {
-            "snd" => {
-                last_frequency = x;
-            }
-            "set" => {
-                regs.insert(reg, val);
-            }
-            "add" => {
-                regs.insert(reg, x + val);
-            }
-            "mul" => {
-                regs.insert(reg, x * val);
-            }
-            "mod" => {
-                regs.insert(reg, x % val);
-            }
-            "rcv" => {
-                if x != 0 {
-                    return Ok(format!("{}", last_frequency));
-                }
-            }
-            "jgz" => {
-                if x > 0 {
-                    pos += val;
-                    continue;
-                }
-            }
-            _ => Err(format!("Invalid instruction: {}", line))?,
-        }
-        pos += 1;
-    }
+    let mut program = Program {
+        pos: 0,
+        regs: HashMap::new(),
+        inbox: VecDeque::new(),
+    };
+    let mut outbox: VecDeque<i64> = VecDeque::new();
+    program.run(&mut outbox, &lines)?;
+    return Ok(format!("{}", outbox.pop_back().ok_or("No value sent")?));
 }
 
 pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
