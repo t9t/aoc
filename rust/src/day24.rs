3bf9a6bfb79cfb0bc0c6e5d2d6e4e48e25793597
aoc <aoc>
2022-03-12 11:00:36 +0100

    rust/2017 day 24 part 2 inefficient

--- rust/src/day24.rs
+++ rust/src/day24.rs
@@ -33,8 +33,63 @@ fn find_max_sum(port: u32, others: &Vec<(u32, u32)>) -> u32 {
     return max_sum;
 }
 
-pub fn part2(_s: &str) -> Result<String, Box<dyn Error>> {
-    return Ok(format!("{}", 5521));
+pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
+    let mut components = Vec::new();
+
+    for line in s.lines() {
+        let mut split = line.split("/");
+        let left = split.next().unwrap().parse::<u32>()?;
+        let right = split.next().unwrap().parse::<u32>()?;
+        components.push((left, right));
+    }
+
+    let all = build_all(0, &Vec::new(), &components);
+    let mut max_len = 0;
+    let mut max_str = 0;
+    for bridge in all {
+        let len = bridge.len();
+        let strength = bridge.iter().map(|(l, r)| l + r).sum();
+
+        if len > max_len {
+            max_len = len;
+            max_str = strength;
+        } else if len == max_len && strength > max_str {
+            max_str = strength;
+        }
+    }
+
+    return Ok(format!("{}", max_str));
+}
+
+fn build_all(
+    port: u32,
+    so_far: &Vec<(u32, u32)>,
+    others: &Vec<(u32, u32)>,
+) -> Vec<Vec<(u32, u32)>> {
+    let mut bridges: Vec<Vec<(u32, u32)>> = Vec::new();
+
+    for (i, &(other_left, other_right)) in others.iter().enumerate() {
+        if other_left != port && other_right != port {
+            continue;
+        }
+
+        let mut next_others = others.clone();
+        next_others.remove(i);
+        let next_port = if other_left == port {
+            other_right
+        } else {
+            other_left
+        };
+
+        let mut next_bridge = so_far.clone();
+        next_bridge.push((other_left, other_right));
+        let all_next = build_all(next_port, &next_bridge, &next_others);
+        bridges.push(next_bridge);
+        for n in all_next {
+            bridges.push(n);
+        }
+    }
+    return bridges;
 }
 
 #[cfg(test)]
@@ -57,6 +112,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT).unwrap(), "5521");
+        assert_eq!(part2(INPUT).unwrap(), "19");
     }
 }
