c0fc706709509cfe8f7b9110530827e5f27a1725
aoc <aoc>
2022-03-12 11:19:08 +0100

    rust/2017 day 24 prettier

--- rust/src/day24.rs
+++ rust/src/day24.rs
@@ -1,39 +1,16 @@
 use std::error::Error;
 
 pub fn part1(s: &str) -> Result<String, Box<dyn Error>> {
-    let mut components = Vec::new();
-
-    for line in s.lines() {
-        let mut split = line.split("/");
-        let left = split.next().unwrap().parse::<u32>()?;
-        let right = split.next().unwrap().parse::<u32>()?;
-        components.push((left, right));
-    }
-
-    let max_sum = find_max_sum(0, &components);
-
+    let (max_sum, _, _) = parse_and_find(s)?;
     return Ok(format!("{}", max_sum));
 }
 
-fn find_max_sum(port: u32, others: &Vec<(u32, u32)>) -> u32 {
-    let mut max_sum = 0;
-    for (i, &(other_left, other_right)) in others.iter().enumerate() {
-        if other_left == port || other_right == port {
-            let mut next_others = others.clone();
-            next_others.remove(i);
-            let next_port = if other_left == port {
-                other_right
-            } else {
-                other_left
-            };
-            let sum = find_max_sum(next_port, &next_others) + other_left + other_right;
-            max_sum = max_sum.max(sum);
-        }
-    }
-    return max_sum;
+pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
+    let (_, _, max_str) = parse_and_find(s)?;
+    return Ok(format!("{}", max_str));
 }
 
-pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
+fn parse_and_find(s: &str) -> Result<(u32, u32, u32), Box<dyn Error>> {
     let mut components = Vec::new();
 
     for line in s.lines() {
@@ -42,12 +19,11 @@ pub fn part2(s: &str) -> Result<String, Box<dyn Error>> {
         let right = split.next().unwrap().parse::<u32>()?;
         components.push((left, right));
     }
-
-    let (_, max_str) = max_len_strength(0, 0, 0, &components);
-    return Ok(format!("{}", max_str));
+    return Ok(find_maxes(0, 0, 0, &components));
 }
 
-fn max_len_strength(port: u32, len: u32, strength: u32, others: &Vec<(u32, u32)>) -> (u32, u32) {
+fn find_maxes(port: u32, len: u32, strength: u32, others: &Vec<(u32, u32)>) -> (u32, u32, u32) {
+    let mut max_sum = 0;
     let mut max_len = len;
     let mut max_str: u32 = strength;
 
@@ -65,15 +41,17 @@ fn max_len_strength(port: u32, len: u32, strength: u32, others: &Vec<(u32, u32)>
         };
 
         let next_strength = strength + other_left + other_right;
-        let (ml, ms) = max_len_strength(next_port, len + 1, next_strength, &next_others);
-        if ml > max_len {
-            max_len = ml;
-            max_str = ms;
-        } else if ml == max_len && ms > max_str {
-            max_str = ms;
+        let (sum, next_max_len, next_max_str) =
+            find_maxes(next_port, len + 1, next_strength, &next_others);
+        max_sum = max_sum.max(sum + other_left + other_right);
+        if next_max_len > max_len {
+            max_len = next_max_len;
+            max_str = next_max_str;
+        } else if next_max_len == max_len && next_max_str > max_str {
+            max_str = next_max_str;
         }
     }
-    return (max_len, max_str);
+    return (max_sum, max_len, max_str);
 }
 
 #[cfg(test)]
