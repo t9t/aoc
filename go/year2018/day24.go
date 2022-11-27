7c820ab2cf15f2dccc2673d4df502f8114f7da03
aoc <aoc>
2022-11-27 10:22:02 +0100

    go/2018 day 24 part 2 dirty

--- go/year2018/day24.go
+++ go/year2018/day24.go
@@ -1,7 +1,6 @@
 package year2018
 
 import (
-	"fmt"
 	"regexp"
 	"sort"
 	"strconv"
@@ -13,21 +12,14 @@ func init() {
 }
 
 func Day24Part1(input string) (string, error) {
-	type group struct {
-		unitCount     int
-		unitHitPoints int
-		weaknesses    []string
-		immunities    []string
-		attackDamage  int
-		damageType    string
-		initiative    int
+	return day24(input, false)
+}
 
-		isInfection bool // false = immune system
+func Day24Part2(input string) (string, error) {
+	return day24(input, true)
+}
 
-		target     *group
-		isTargeted bool
-	}
-
+func day24(input string, part2 bool) (string, error) {
 	parseTypes := func(s string) []string {
 		if s == "" {
 			return []string{}
@@ -54,10 +46,10 @@ func Day24Part1(input string) (string, error) {
 
 	// 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
 	re := regexp.MustCompile(`(\d+) units each with (\d+) hit points( \(.+\))? with an attack that does (\d+) (\w+) damage at initiative (\d+)`)
-	parseGroup := func(line string) (group, error) {
+	parseGroup := func(line string) (day24group, error) {
 		matches := re.FindStringSubmatch(line)
 		nums := make([]int, 4)
-		g := group{}
+		g := day24group{}
 		for i, s := range []string{matches[1], matches[2], matches[4], matches[6]} {
 			if n, err := strconv.Atoi(s); err != nil {
 				return g, err
@@ -82,7 +74,7 @@ func Day24Part1(input string) (string, error) {
 	}
 
 	lines := strings.Split(input, "\n")
-	groups := make([]*group, 0)
+	inputGroups := make([]*day24group, 0)
 	isInfection := false
 	for _, line := range lines {
 		if line == "Immune System:" {
@@ -98,102 +90,142 @@ func Day24Part1(input string) (string, error) {
 			return "", err
 		} else {
 			g.isInfection = isInfection
-			groups = append(groups, &g)
+			if !g.isInfection {
+				//g.attackDamage += boost
+			}
+			inputGroups = append(inputGroups, &g)
 		}
 	}
 
-	for {
-		// Reset
-		newGroups := make([]*group, 0)
-		infectionFound, immuneFound := false, false
-		for _, g := range groups {
-			if g.unitCount > 0 {
-				g.target, g.isTargeted = nil, false
-				newGroups = append(newGroups, g)
-				if g.isInfection {
-					infectionFound = true
-				} else {
-					immuneFound = true
+	for boost := 0; ; boost++ {
+		solutionFound, unitCount := func() (bool, int) {
+			groups := make([]*day24group, len(inputGroups))
+			for i, g := range inputGroups {
+				groupCopy := *g
+				if !g.isInfection {
+					groupCopy.attackDamage += boost
 				}
+				groups[i] = &groupCopy
 			}
-		}
-		if !infectionFound || !immuneFound {
-			totalUnitCount := 0
-			for _, g := range newGroups {
-				totalUnitCount += g.unitCount
-			}
-			return strconv.Itoa(totalUnitCount), nil
-		}
-		groups = newGroups
 
-		// Target selection phase
-		sort.Slice(groups, func(i, j int) bool {
-			l, r := groups[i], groups[j]
-			lep, rep := l.unitCount*l.attackDamage, r.unitCount*r.attackDamage
-			if lep == rep {
-				return l.initiative > r.initiative
-			}
-			return lep > rep
-		})
-		for _, g := range groups {
-			candidates := make([]*group, 0)
-			for _, candidate := range groups {
-				if g.isInfection == candidate.isInfection {
-					continue
-				} else if candidate.isTargeted {
-					continue
-				} else if contains(candidate.immunities, g.damageType) {
-					continue
+			for {
+				// Reset
+				newGroups := make([]*day24group, 0)
+				infectionFound, immuneFound := false, false
+				for _, g := range groups {
+					if g.unitCount > 0 {
+						g.target, g.isTargeted = nil, false
+						newGroups = append(newGroups, g)
+						if g.isInfection {
+							infectionFound = true
+						} else {
+							immuneFound = true
+						}
+					}
 				}
-				candidates = append(candidates, candidate)
-			}
-			if len(candidates) == 0 {
-				continue
-			}
-			sort.Slice(candidates, func(i, j int) bool {
-				/* If an attacking group is considering two defending groups to which it would deal equal damage, it chooses
-				to target the defending group with the largest effective power; if there is still a tie, it chooses the
-				defending group with the highest initiative. If it cannot deal any defending groups damage, it does not
-				choose a target. Defending groups can only be chosen as a target by one attacking group.*/
-				l, r := candidates[i], candidates[j]
-				ldmg, rdmg := g.attackDamage, g.attackDamage
-				if contains(l.weaknesses, g.damageType) {
-					ldmg *= 2
+				if !infectionFound || !immuneFound {
+					if part2 && !immuneFound {
+						return false, 0
+					}
+
+					totalUnitCount := 0
+					for _, g := range newGroups {
+						totalUnitCount += g.unitCount
+					}
+					return true, totalUnitCount
 				}
-				if contains(r.weaknesses, g.damageType) {
-					rdmg *= 2
-				}
-				if ldmg == rdmg {
+				groups = newGroups
+
+				// Target selection phase
+				sort.Slice(groups, func(i, j int) bool {
+					l, r := groups[i], groups[j]
 					lep, rep := l.unitCount*l.attackDamage, r.unitCount*r.attackDamage
 					if lep == rep {
 						return l.initiative > r.initiative
 					}
 					return lep > rep
+				})
+				totalTargets := 0
+				for _, g := range groups {
+					candidates := make([]*day24group, 0)
+					for _, candidate := range groups {
+						if g.isInfection == candidate.isInfection {
+							continue
+						} else if candidate.isTargeted {
+							continue
+						} else if contains(candidate.immunities, g.damageType) {
+							continue
+						}
+						candidates = append(candidates, candidate)
+					}
+					if len(candidates) == 0 {
+						continue
+					}
+					sort.Slice(candidates, func(i, j int) bool {
+						/* If an attacking group is considering two defending groups to which it would deal equal damage, it chooses
+						to target the defending group with the largest effective power; if there is still a tie, it chooses the
+						defending group with the highest initiative. If it cannot deal any defending groups damage, it does not
+						choose a target. Defending groups can only be chosen as a target by one attacking group.*/
+						l, r := candidates[i], candidates[j]
+						ldmg, rdmg := g.attackDamage, g.attackDamage
+						if contains(l.weaknesses, g.damageType) {
+							ldmg *= 2
+						}
+						if contains(r.weaknesses, g.damageType) {
+							rdmg *= 2
+						}
+						if ldmg == rdmg {
+							lep, rep := l.unitCount*l.attackDamage, r.unitCount*r.attackDamage
+							if lep == rep {
+								return l.initiative > r.initiative
+							}
+							return lep > rep
+						}
+						return ldmg > rdmg
+					})
+					g.target = candidates[0]
+					candidates[0].isTargeted = true
+					totalTargets++
 				}
-				return ldmg > rdmg
-			})
-			g.target = candidates[0]
-			candidates[0].isTargeted = true
-		}
+				if totalTargets == 0 {
+					// Couldn't assign any targets, deadlock, ignore this round
+					return false, 0
+				}
 
-		// Attacking phase
-		sort.Slice(groups, func(i, j int) bool {
-			return groups[i].initiative > groups[j].initiative
-		})
+				// Attacking phase
+				sort.Slice(groups, func(i, j int) bool {
+					return groups[i].initiative > groups[j].initiative
+				})
 
-		for _, g := range groups {
-			if g.unitCount <= 0 || g.target == nil {
-				continue
+				for _, g := range groups {
+					if g.unitCount <= 0 || g.target == nil {
+						continue
+					}
+					dmg := g.unitCount * g.attackDamage
+					if contains(g.target.weaknesses, g.damageType) {
+						dmg *= 2
+					}
+					g.target.unitCount -= dmg / g.target.unitHitPoints
+				}
 			}
-			dmg := g.unitCount * g.attackDamage
-			if contains(g.target.weaknesses, g.damageType) {
-				dmg *= 2
-			}
-			g.target.unitCount -= dmg / g.target.unitHitPoints
+		}()
+		if solutionFound {
+			return strconv.Itoa(unitCount), nil
 		}
 	}
 }
 
-func Day24Part2(input string) (string, error) {
-	return "", fmt.Errorf("Day 24 part 2 not implemented")
+type day24group struct {
+	unitCount     int
+	unitHitPoints int
+	weaknesses    []string
+	immunities    []string
+	attackDamage  int
+	damageType    string
+	initiative    int
+
+	isInfection bool // false = immune system
+
+	target     *day24group
+	isTargeted bool
 }
