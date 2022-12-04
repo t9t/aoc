3b1e743300e3b790bffd9616a90b9f11bc199b2a
aoc <aoc>
2022-12-04 16:17:36 +0100

    haskell/2022 day 4 part 1

---
+++ haskell/2022-day-4-part-1.hs
@@ -0,0 +1,12 @@
+import Data.Char
+import Data.List
+
+breakOn c s = (l, tail r) where (l, r) = break (c==) s
+mapBoth fn (l, r) = (fn l, fn r)
+eitherContainsOther (l, r) = (l `isInfixOf` r) || (r `isInfixOf` l)
+toRange (l, r) = [l..r]
+parse line = mapBoth (\p -> toRange(mapBoth (\s -> (read s :: Integer)) (breakOn '-' p))) (breakOn ',' line)
+
+main = do
+    c <- getContents
+    print $ length $ filter eitherContainsOther $ map parse $ lines c
