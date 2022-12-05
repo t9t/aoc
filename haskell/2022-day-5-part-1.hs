85c236f12ca27ab524927828582a818cdddb2ab6
aoc <aoc>
2022-12-05 22:28:11 +0100

    haskell/2022 day 5 part 1

---
+++ haskell/2022-day-5-part-1.hs
@@ -0,0 +1,55 @@
+import Data.Char
+import Data.List
+
+breakOn c s = (l, tail r) where (l, r) = break (c==) s
+cleanse s = [c | c <- s, c `elem` ['A'..'Z']]
+zipAll ss = if length ss == 1 then head ss else zipWith (++) (head ss) (zipAll (tail ss))
+toInt s = (read s :: Int)
+
+partitionTo n s
+    | len <= n  = [s]
+    | otherwise = l : partitionTo n s'
+    where len = length s
+          (l, s') = splitAt n s
+
+parse s = parseInputTuple $ breakOn "" $ lines s
+parseStacks l = zipAll $ map (map cleanse . partitionTo 4) $ init l 
+
+parseInputTuple :: ([String], [String]) -> ([String], [(Int, Int, Int)])
+parseInputTuple (stacks, procedures) = (parseStacks stacks, map parseProcedure procedures)
+readTops stack = map head stack
+
+
+
+parseProcedure :: String -> (Int, Int, Int)
+parseProcedure s = (toInt (w !! 1), toInt (w !! 3), toInt (w !! 5))
+    where w = words s
+
+
+applyProcedure :: (String, Int) -> Char -> Int -> Int -> String
+applyProcedure (stack, number) fromChar from to
+    | from == number = tail stack
+    | to == number   = fromChar : stack
+    | otherwise      = stack
+
+move :: [String] -> Int -> Int -> [String]
+move stacks from to = 
+    let withIndices = zip stacks [1..]
+    in [applyProcedure tup (head (stacks !! (from-1))) from to | tup <- withIndices]
+
+-- move 2 from 4 to 9
+-- (2,4,9)
+process :: [String] -> (Int, Int, Int) -> [String]
+process stacks (0, _, _) = stacks
+process stacks (1, from, to) = move stacks from to
+process stacks (n, from, to) = process (move stacks from to) ((n-1), from, to)
+
+processAll :: ([String], [(Int, Int, Int)]) -> [String]
+processAll (stacks, []) = stacks
+processAll (stacks, procedures) = 
+    let processed = process stacks (head procedures)
+    in  processAll (processed, (tail procedures))
+
+main = do
+    c <- getContents
+    print $ readTops $ processAll (parse c)
