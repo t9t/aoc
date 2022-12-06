2c9e5d4d6ab7c78c208c21c3fed5a4ca64edfe08
aoc <aoc>
2022-12-06 20:44:42 +0100

    haskell/2022 day 6 splitAt

--- haskell/2022-day-6-part-1.hs
+++ haskell/2022-day-6-part-1.hs
@@ -6,9 +6,10 @@ areAllDifferent s = (nub s) == s
 brrrr :: String -> String -> Int -> Int
 brrrr l r n
     | null r            = error "no solution found"
-    | null l            = brrrr (take 4 r) (drop 4 r) 4
+    | null l            = brrrr l' r' 4
     | areAllDifferent l = n
     | otherwise         = brrrr ((tail l) ++ [(head r)]) (tail r) n+1
+    where (l', r') = splitAt 14 r
 
 main = do
     c <- getContents
--- haskell/2022-day-6-part-2.hs
+++ haskell/2022-day-6-part-2.hs
@@ -6,9 +6,10 @@ areAllDifferent s = (nub s) == s
 brrrr :: String -> String -> Int -> Int
 brrrr l r n
     | null r            = error "no solution found"
-    | null l            = brrrr (take 14 r) (drop 14 r) 14
+    | null l            = brrrr l' r' 14
     | areAllDifferent l = n
     | otherwise         = brrrr ((tail l) ++ [(head r)]) (tail r) n+1
+    where (l', r') = splitAt 14 r
 
 main = do
     c <- getContents
