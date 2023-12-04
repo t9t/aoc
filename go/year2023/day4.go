4748c39e2ac6a1bf51d60868efae9988ee48e418
aoc <aoc>
2023-12-04 18:04:58 +0100

    go/2023 day 4 part 2

--- go/year2023/day4.go
+++ go/year2023/day4.go
@@ -64,5 +64,87 @@ func Day4Part1(input string) (string, error) {
 }
 
 func Day4Part2(input string) (string, error) {
-	return "", fmt.Errorf("Day 4 part 2 not implemented")
+	type card struct {
+		matchingNumbers int
+		count           int
+	}
+	cards := make([]*card, 0)
+	cardIndex := 0
+	for _, line := range strings.Split(input, "\n") {
+		cardNum, lists, found := strings.Cut(line, ": ")
+		if !found {
+			return "", fmt.Errorf("invalid line (no ': '): %s", line)
+		}
+
+		cardNumber, err := strconv.Atoi(strings.TrimSpace(strings.TrimPrefix(cardNum, "Card ")))
+		if err != nil {
+			return "", err
+		}
+
+		if cardNumber != cardIndex+1 {
+			return "", fmt.Errorf("assumption violated, cardNumber %d does not match cardIndex %d+1", cardNumber, cardIndex)
+		}
+
+		winning, owned, found := strings.Cut(lists, " | ")
+		if !found {
+			return "", fmt.Errorf("invalid line (no ' | '): %s", line)
+		}
+
+		toNumbers := func(s string) (map[int]struct{}, error) {
+			numbers := make(map[int]struct{})
+			for _, item := range strings.Split(strings.TrimSpace(s), " ") {
+				if item == "" {
+					continue
+				}
+				n, err := strconv.Atoi(strings.TrimSpace(item))
+				if err != nil {
+					return nil, err
+				}
+				numbers[n] = struct{}{}
+			}
+			return numbers, nil
+		}
+
+		winningNumbers, err := toNumbers(winning)
+		if err != nil {
+			return "", err
+		}
+
+		ownedNumbers, err := toNumbers(owned)
+		if err != nil {
+			return "", err
+		}
+
+		matching := 0
+		for n := range ownedNumbers {
+			if _, found := winningNumbers[n]; found {
+				matching += 1
+			}
+		}
+		cards = append(cards, &card{
+			matchingNumbers: matching,
+			count:           1,
+		})
+		cardIndex += 1
+	}
+
+	l := len(cards)
+	for i, card := range cards {
+		if card.matchingNumbers == 0 {
+			continue
+		}
+
+		for d := i + 1; d <= i+card.matchingNumbers; d++ {
+			if d == l {
+				break
+			}
+			cards[d].count += card.count
+		}
+	}
+
+	totalCardCount := 0
+	for _, card := range cards {
+		totalCardCount += card.count
+	}
+	return strconv.Itoa(totalCardCount), nil
 }
