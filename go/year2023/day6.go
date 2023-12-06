79f6e6e046ca9622290eab8043853a0aa8501511
aoc <aoc>
2023-12-06 15:31:19 +0100

    go/2023 day 6 simplify

--- go/year2023/day6.go
+++ go/year2023/day6.go
@@ -13,7 +13,6 @@ func init() {
 
 func Day6Part1(input string) (string, error) {
 	re := regexp.MustCompile(`(?m)(\d+)`)
-	lines := strings.Split(input, "\n")
 	toNumbers := func(s string) ([]int, error) {
 		var ret []int
 		for _, m := range re.FindAllString(s, -1) {
@@ -26,37 +25,35 @@ func Day6Part1(input string) (string, error) {
 		return ret, nil
 	}
 
-	times, err := toNumbers(lines[0])
-	if err != nil {
-		return "", fmt.Errorf("invalid line %s: %w\n", lines[0], err)
-	}
-	distances, err := toNumbers(lines[1])
-	if err != nil {
-		return "", fmt.Errorf("invalid line %s: %w\n", lines[1], err)
-	}
-
-	return day6(times, distances)
+	return day6(input, toNumbers)
 }
 
 func Day6Part2(input string) (string, error) {
-	lines := strings.Split(input, "\n")
-	parseLine := func(s string) (int, error) {
-		return strconv.Atoi(strings.ReplaceAll(strings.Split(s, ":")[1], " ", ""))
+	parseLine := func(s string) ([]int, error) {
+		n, err := strconv.Atoi(strings.ReplaceAll(strings.Split(s, ":")[1], " ", ""))
+		if err != nil {
+			return nil, err
+		}
+		return []int{n}, nil
 	}
 
-	times, err := parseLine(lines[0])
-	if err != nil {
-		return "", fmt.Errorf("invalid line %s: %w\n", lines[0], err)
-	}
-	distances, err := parseLine(lines[1])
-	if err != nil {
-		return "", fmt.Errorf("invalid line %s: %w\n", lines[1], err)
-	}
-
-	return day6([]int{times}, []int{distances})
+	return day6(input, parseLine)
 }
 
-func day6(times, distances []int) (string, error) {
+func day6(input string, parseFunc func(string) ([]int, error)) (string, error) {
+	lines := strings.Split(input, "\n")
+	if len(lines) != 2 {
+		return "", fmt.Errorf("invalid input (expected 2 lines but got %d): %s", len(lines), input)
+	}
+	times, err := parseFunc(lines[0])
+	if err != nil {
+		return "", fmt.Errorf("invalid Times line %s: %w", lines[0], err)
+	}
+	distances, err := parseFunc(lines[1])
+	if err != nil {
+		return "", fmt.Errorf("invalid Distances line %s: %w", lines[1], err)
+	}
+
 	mul := 1
 	for i, time := range times {
 		dist := distances[i]
