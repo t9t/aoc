27d49786eab3840a0704e56cd79ce2353e2c3a57
aoc <aoc>
2023-12-07 21:27:53 +0100

    go/2023 day 7 part 2 dirty

--- go/year2023/day7.go
+++ go/year2023/day7.go
@@ -77,5 +77,150 @@ func Day7Part1(input string) (string, error) {
 }
 
 func Day7Part2(input string) (string, error) {
-	return "", fmt.Errorf("Day 7 part 2 not implemented")
+	// A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+	cardScores := make(map[byte]int)
+	// J now in front
+	for i, card := range []byte{'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'} {
+		cardScores[card] = i
+	}
+
+	const (
+		fiveOfAKind  = 0
+		fourOfAKind  = 1
+		fullHouse    = 2
+		threeOfAKind = 3
+		twoPair      = 4
+		onePair      = 5
+		highCard     = 6
+	)
+
+	scoreHand := func(s string) int {
+		s = strings.Split(s, " ")[0]
+		counts := make(map[rune]int)
+		maxCount := 0
+		for _, c := range s {
+			nn := counts[c] + 1
+			counts[c] = nn
+			if nn > maxCount {
+				maxCount = nn
+			}
+		}
+		if maxCount == 5 {
+			return fiveOfAKind // Five of a kind
+		} else if maxCount == 4 {
+			return fourOfAKind
+		} else if len(counts) == 2 {
+			return fullHouse
+		} else if maxCount == 3 {
+			return threeOfAKind
+		} else if len(counts) == 3 {
+			return twoPair
+		} else if maxCount == 2 {
+			return onePair
+		} else if len(counts) == 5 {
+			return highCard
+		}
+		panic(fmt.Sprintf("invalid hand? %s -> %+v", s, counts))
+	}
+
+	scoreHandJokerized := func(s string) int {
+		jokerCount := strings.Count(s, "J")
+		if jokerCount == 0 {
+			return scoreHand(s)
+		}
+		maxScore := scoreHand(s)
+		for card := range cardScores {
+			if card == 'J' {
+				continue
+			}
+			rep := strings.ReplaceAll(s, "J", string([]byte{card}))
+			score := scoreHand(rep)
+			if score < maxScore {
+				maxScore = score
+			}
+		}
+		return maxScore
+	}
+
+	jokerize := func(s string, score int) int {
+		jokerCount := strings.Count(s, "J")
+		if jokerCount == 0 {
+			return score
+		}
+
+		if score == fiveOfAKind {
+			return fiveOfAKind
+		}
+		if score == fourOfAKind {
+			// XJJJJ or XXXXJ
+			return fiveOfAKind
+		}
+		if score == fullHouse {
+			// XXJJJ or JJXXX
+			return fiveOfAKind
+		}
+		if score == threeOfAKind {
+			// XYJJJ or XYYYJ or XXXYJ
+			return fourOfAKind
+		}
+		if score == twoPair {
+			if jokerCount == 2 {
+				// 2 others + 2 jokers = 4 of a kind
+				return fourOfAKind
+			} else {
+				if jokerCount != 1 {
+					panic(fmt.Sprintf("%s - %d - %d", s, score, jokerCount))
+				}
+				// 2 others + 1 joker = 3 of a kind
+				return threeOfAKind
+			}
+		}
+		if score == onePair {
+			if jokerCount == 2 {
+				// 1 of 3 different ones + 2 jokers = 3 of a kind
+				return threeOfAKind
+			} else {
+				if jokerCount != 1 {
+					panic(fmt.Sprintf("%s - %d - %d", s, score, jokerCount))
+				}
+				// 2 of a pair + 1 joker = 3 of a kind
+				return threeOfAKind
+			}
+		}
+		if score == highCard {
+			// ABCDJ -> ABCJJ
+			return twoPair
+		}
+		panic(fmt.Sprintf("cannot jokerize %s", s))
+	}
+
+	func(any) {}(jokerize)
+
+	lines := strings.Split(input, "\n")
+	sort.Slice(lines, func(i, j int) bool {
+		l, r := lines[i], lines[j]
+		ls, rs := scoreHandJokerized(l), scoreHandJokerized(r)
+		if ls == rs {
+			for k := 0; k < len(l); k++ {
+				lc, rc := l[k], r[k]
+				if lc == rc {
+					continue
+				}
+				lcs, rcs := cardScores[lc], cardScores[rc]
+				return lcs < rcs
+			}
+			panic("woops")
+		}
+		return ls > rs
+	})
+
+	sum := 0
+	for i, line := range lines {
+		bid, err := strconv.Atoi(strings.Split(line, " ")[1])
+		if err != nil {
+			return "", fmt.Errorf("invalid line %s: %w", line, err)
+		}
+		sum += (i + 1) * bid
+	}
+	return strconv.Itoa(sum), nil
 }
--- go/year2023/day7_test.go
+++ go/year2023/day7_test.go
@@ -4,18 +4,20 @@ import (
 	"testing"
 )
 
-func Test_Day7Part1(t *testing.T) {
-	basicMultiTest(t, Day7Part1, []testInput{
-		{`32T3K 765
+const day7input = `32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
-QQQJA 483`, "6440"},
+QQQJA 483`
+
+func Test_Day7Part1(t *testing.T) {
+	basicMultiTest(t, Day7Part1, []testInput{
+		{day7input, "6440"},
 	})
 }
 
 func Test_Day7Part2(t *testing.T) {
 	basicMultiTest(t, Day7Part2, []testInput{
-		{"", ""},
+		{day7input, "5905"},
 	})
 }
