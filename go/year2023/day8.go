b5eea2ea7ae0f03b00ff3a202a70b0e1134bbfd4
aoc <aoc>
2023-12-08 21:30:30 +0100

    go/2023 day 8 part 2

--- go/year2023/day8.go
+++ go/year2023/day8.go
@@ -49,5 +49,71 @@ func Day8Part1(input string) (string, error) {
 }
 
 func Day8Part2(input string) (string, error) {
-	return "", fmt.Errorf("Day 8 part 2 not implemented")
+	chunks := strings.Split(input, "\n\n")
+	if len(chunks) != 2 {
+		return "", fmt.Errorf("invalid input")
+	}
+
+	type node struct{ left, right string }
+
+	instructions := chunks[0]
+	re := regexp.MustCompile(`(\w+) = \((\w+), (\w+)\)`)
+	nodes := make(map[string]node, 0)
+	for _, m := range re.FindAllStringSubmatch(chunks[1], -1) {
+		nodes[m[1]] = node{left: m[2], right: m[3]}
+	}
+
+	curs := make([]node, 0)
+	for el, node := range nodes {
+		if strings.HasSuffix(el, "A") {
+			curs = append(curs, node)
+		}
+	}
+
+	minSteps := make(map[int]int)
+	index, steps := 0, 0
+	for {
+		steps += 1
+		r := instructions[index]
+		index += 1
+		if index == len(instructions) {
+			index = 0
+		}
+
+		nexts := make([]node, len(curs))
+		for i, cur := range curs {
+			next := cur.left
+			if r == 'R' {
+				next = cur.right
+			}
+			nexts[i] = nodes[next]
+			if strings.HasSuffix(next, "Z") {
+				if _, f := minSteps[i]; !f {
+					minSteps[i] = steps
+				}
+			}
+		}
+
+		if len(minSteps) == len(nexts) {
+			break
+		}
+
+		curs = nexts
+	}
+
+	p := minSteps[0]
+	n := p
+	for i := 1; i < len(minSteps); i++ {
+		k := minSteps[i]
+		for {
+			if n%k == 0 {
+				p = n
+				break
+			} else {
+				n += p
+			}
+		}
+	}
+
+	return strconv.Itoa(n), nil
 }
--- go/year2023/day8_test.go
+++ go/year2023/day8_test.go
@@ -25,6 +25,15 @@ ZZZ = (ZZZ, ZZZ)`, "6"},
 
 func Test_Day8Part2(t *testing.T) {
 	basicMultiTest(t, Day8Part2, []testInput{
-		{"", ""},
+		{`LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`, "6"},
 	})
 }
