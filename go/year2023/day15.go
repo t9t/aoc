85414c901d470429c1796ac7eded7d30b512c4ae
aoc <aoc>
2023-12-15 20:10:43 +0100

    go/2023 day 15 part 2

--- go/year2023/day15.go
+++ go/year2023/day15.go
@@ -27,5 +27,71 @@ func Day15Part1(input string) (string, error) {
 }
 
 func Day15Part2(input string) (string, error) {
-	return "", fmt.Errorf("Day 15 part 2 not implemented")
+	steps := strings.Split(strings.ReplaceAll(input, "\n", ""), ",")
+	type lens struct {
+		label       string
+		focalLength int
+	}
+	var boxes [256][]lens
+
+	for _, step := range steps {
+		var label string
+		var operation byte
+		var focalLength int
+		if strings.HasSuffix(step, "-") {
+			operation = '-'
+			label = strings.TrimSuffix(step, "-")
+		} else {
+			operation = '='
+			parts := strings.Split(step, "=")
+			if len(parts) != 2 {
+				return "", fmt.Errorf("invalid step %s (expected 2 parts but got %d)", step, len(parts))
+			}
+			label = parts[0]
+			var err error
+			if focalLength, err = strconv.Atoi(parts[1]); err != nil {
+				return "", fmt.Errorf("invalid step %s: %w", step, err)
+			}
+		}
+
+		hash := 0
+		for _, r := range label {
+			hash += int(r)
+			hash *= 17
+			hash %= 256
+		}
+		box := boxes[hash]
+		if operation == '-' {
+			newBox := make([]lens, 0)
+			for _, item := range box {
+				if item.label != label {
+					newBox = append(newBox, item)
+				}
+			}
+			boxes[hash] = newBox
+		} else if operation == '=' {
+			newBox, found := make([]lens, 0), false
+			for _, item := range box {
+				if item.label == label {
+					newBox = append(newBox, lens{label: label, focalLength: focalLength})
+					found = true
+				} else {
+					newBox = append(newBox, item)
+				}
+			}
+			if !found {
+				newBox = append(newBox, lens{label: label, focalLength: focalLength})
+			}
+			boxes[hash] = newBox
+		}
+	}
+
+	sum := 0
+	for i, box := range boxes {
+		for slotNum, lens := range box {
+			sum += (i + 1) * (slotNum + 1) * lens.focalLength
+		}
+	}
+
+	return strconv.Itoa(sum), nil
 }
--- go/year2023/day15_test.go
+++ go/year2023/day15_test.go
@@ -12,6 +12,6 @@ func Test_Day15Part1(t *testing.T) {
 
 func Test_Day15Part2(t *testing.T) {
 	basicMultiTest(t, Day15Part2, []testInput{
-		{"", ""},
+		{"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", "145"},
 	})
 }
